f36c06705a91a2d2cb2b2876160d754d1e3d95a9
luizcarlosdk <luizcarlosdk@hotmail.com>
2022-04-28 14:39:34 -0300

    Add validator to v-textarea
    Signed-off-by: luizcarlosdk <luizcarlosdk@hotmail.com>

--- frontend/pages/conexao.vue
+++ frontend/pages/conexao.vue
@@ -1,14 +1,5 @@
 <template>
   <div class="background">
-    <div>
-      <Panel
-        title="Conexão USP"
-        description="Este Programa da Agência USP de Inovação tem por objetivo oferecer a intermediação e o contato entre parceiros (empresas, entidades sem fins lucrativos e governo) e os pesquisadores da Universidade de São Paulo.
-                    Neste programa, os parceiros apresentam suas demandas para que identifiquemos pesquisadores na Universidade que tenham a soluções ou propostas de projetos de pesquisa que atendam estas necessidades."
-        no-search
-      />
-    </div>
-
     <v-row style="background: white">
       <v-col>
         <v-img
@@ -20,6 +11,15 @@
         />
       </v-col>
     </v-row>
+    <div>
+      <Panel
+        title="Conexão USP"
+        description="Este Programa da Agência USP de Inovação tem por objetivo oferecer a intermediação e o contato entre parceiros (empresas, entidades sem fins lucrativos e governo) e os pesquisadores da Universidade de São Paulo.
+                    Neste programa, os parceiros apresentam suas demandas para que identifiquemos pesquisadores na Universidade que tenham a soluções ou propostas de projetos de pesquisa que atendam estas necessidades."
+        no-search
+      />
+    </div>
+
 
     <v-container class="pa-10">
       <v-form ref="form">
@@ -484,34 +484,42 @@ export default {
       const org = Object.keys(this.conexao.org);
       const demand = Object.keys(this.conexao.demand);
       const cnae = Object.keys(this.conexao.demand.cnae);
+      const text = this.conexao.demand.description
       let isvalid = false;
       let errors = [];
 
       personal.forEach((e, index) => {
         if (this.conexao.personal[e] == "") {
-          errors.push("personal_"+Object.keys(this.conexao.personal)[index]);
+          errors.push("personal_" + Object.keys(this.conexao.personal)[index]);
         }
       });
 
       org.forEach((e, index) => {
         if (this.conexao.org[e] == "") {
-          errors.push("organization_"+Object.keys(this.conexao.org)[index]);
+          errors.push("organization_" + Object.keys(this.conexao.org)[index]);
         }
       });
 
       demand.forEach((e, index) => {
         if (e != "cnae") {
           if (this.conexao.demand[e] == "") {
-            errors.push("demand_"+Object.keys(this.conexao.demand)[index]);
+            errors.push("demand_" + Object.keys(this.conexao.demand)[index]);
           }
         }
       });
 
       cnae.forEach((e, index) => {
         if (this.conexao.demand.cnae[e] == "") {
-          errors.push("cnae_"+Object.keys(this.conexao.demand.cnae)[index]);
+          errors.push("cnae_" + Object.keys(this.conexao.demand.cnae)[index]);
         }
       });
+
+      if(text.length == ""){
+        errors.push("Campo de texto vazio")
+      }
+      if(text.split(" ").length > 500){
+        errors.push("Limite de 500 caracteres")
+      }
 
       if (errors.length == 0) {
         isvalid = true;
@@ -536,7 +544,6 @@ export default {
           console.log(error);
         }
       } else {
-        console.log(errors);
         alert(
           "Há erros no formulário, por favor corrijia-os e submeta novamente"
         );
