4809ea6177144dbc71d4f04e7d3253d812f63bdf
João Daniel <jotaf.daniel@gmail.com>
2022-06-22 10:18:22 -0300

    swap disciplines filtering locally for remotelly
    Signed-off-by: João Daniel <jotaf.daniel@gmail.com>
    Signed-off-by: Luiz Carlos <luizcarlosdk@hotmail.com>
    Signed-off-by: Briza Mel <brizamel.dias@gmail.com>
    Signed-off-by: Marilia Dicezare <mariliatd@gmail.com>

---
+++ frontend/plugins/Catalog/DisciplineAdapter.js
@@ -0,0 +1,35 @@
+function DisciplineAdapter(axios) {
+  const baseURL = "http://localhost:8080"
+
+  return {
+    requestData: async () => {
+      const url = baseURL + "/disciplines"
+
+      const { disciplines } = await axios.$get(url)
+
+      return disciplines
+    },
+
+    filterData: async (params) => {
+      const url = baseURL + "/disciplines"
+
+      const stringParams = Object
+        .keys(params)
+        .filter((key) => params[key])
+        .map((key) => {
+          const value = params[key]
+          return `${key}=${value}`
+        })
+        .join("&")
+
+      const { disciplines } = await axios.$get(`${url}?${stringParams}`)
+
+      return disciplines
+    }
+  }
+}
+
+export default (context, inject) => {
+  const adapter = DisciplineAdapter(context.$axios)
+  inject("DisciplineAdapter", adapter)
+}
