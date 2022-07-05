5aa03355a71ece1e96c22ec5d425d62038ba3180
João Daniel <jotaf.daniel@gmail.com>
2022-07-05 17:55:53 -0300

    fix name and structure of discipline category

--- server/src/main/kotlin/br/usp/inovacao/hubusp/catalog/domain/Discipline.kt
+++ server/src/main/kotlin/br/usp/inovacao/hubusp/catalog/domain/Discipline.kt
@@ -1,9 +1,19 @@
 package br.usp.inovacao.hubusp.catalog.domain
 
-@kotlinx.serialization.Serializable
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Category(
+    val innovation: Boolean,
+    val business: Boolean,
+    val entrepreneurship: Boolean,
+    val intellectual_property: Boolean
+)
+
+@Serializable
 data class Discipline(
     val name: String,
-    val categories: List<String>,
+    val category: Category,
     val unity: String,
     val campus: String,
     val level: String,
