7b984b1666a50f9e6a1b772eb12fc37572800e70
João Daniel <jotaf.daniel@gmail.com>
2022-07-05 17:55:02 -0300

    connect mongodb to existing database

--- server/src/main/kotlin/br/usp/inovacao/hubusp/catalog/CatalogModule.kt
+++ server/src/main/kotlin/br/usp/inovacao/hubusp/catalog/CatalogModule.kt
@@ -8,7 +8,7 @@ import io.ktor.server.application.Application
 import org.litote.kmongo.getCollection
 
 fun Application.configureCatalogModule(db: MongoDatabase) {
-    val disciplineCollection = db.getCollection<Discipline>()
+    val disciplineCollection = db.getCollection<Discipline>("disciplines")
 
     val disciplineService = DisciplineService(disciplineCollection)
     configureCatalogRouting(disciplineService)
