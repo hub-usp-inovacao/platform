a1f997784bad843661b30c7a9890c59ed75fea32
João Daniel <jotaf.daniel@gmail.com>
2022-07-05 17:56:20 -0300

    fix db query filter generation

--- server/src/main/kotlin/br/usp/inovacao/hubusp/catalog/domain/DisciplineQueryFilter.kt
+++ server/src/main/kotlin/br/usp/inovacao/hubusp/catalog/domain/DisciplineQueryFilter.kt
@@ -4,9 +4,8 @@ import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 
 @Suppress("unused")
-@kotlinx.serialization.Serializable
 class DisciplineQueryFilter private constructor(
-    val categories: Map<String, List<String>>? = null,
+    val category: Map<String, List<Map<String, Boolean>>>? = null,
     val unity: String? = null,
     val campus: String? = null,
     val level: String? = null,
@@ -15,15 +14,54 @@ class DisciplineQueryFilter private constructor(
     companion object {
         fun from(params: Map<String, List<String>>): DisciplineQueryFilter {
             return DisciplineQueryFilter(
-                categories = if (params["categories"] != null) mapOf("\$in" to params["categories"]!![0].split(',')) else null,
+                category = parseCategories(params["categories"]),
                 unity = params["unity"]?.get(0),
                 campus = params["campus"]?.get(0),
                 level = params["level"]?.get(0),
                 nature = params["nature"]?.get(0)
             )
         }
+
+        private fun parseCategories(categories: List<String>?): Map<String, List<Map<String, Boolean>>>? {
+            if (categories == null) return null
+
+            val ptBrCategoryList = categories[0].split(',')
+
+            val conditions = mutableListOf<Map<String, Boolean>>()
+
+            if (ptBrCategoryList.contains("Negócios")) conditions.add(
+                mapOf("category.business" to true)
+            )
+
+            if (ptBrCategoryList.contains("Empreendedorismo")) conditions.add(
+                mapOf("category.entrepreneurship" to true)
+            )
+
+            if (ptBrCategoryList.contains("Propriedade Intelectual")) conditions.add(
+                mapOf("category.intellectual_property" to true)
+            )
+
+            if (ptBrCategoryList.contains("Inovação")) conditions.add(
+                mapOf("category.innovation" to true)
+            )
+
+            return mapOf("\$or" to conditions)
+        }
     }
 
-    fun toJson(): String = Json
-        .encodeToString(this)
+    fun toJson(): String {
+        val rawJson: MutableList<String> = mutableListOf()
+
+        if (unity != null) rawJson.add("unity:\"$unity\"")
+        if (campus != null) rawJson.add("campus:\"$campus\"")
+        if (level != null) rawJson.add("level:\"$level\"")
+        if (nature != null) rawJson.add("nature:\"$nature\"")
+
+        if (category != null) {
+            val conditionsJson = Json.encodeToString(category["\$or"])
+            rawJson.add("\$or:$conditionsJson")
+        }
+
+        return "{" + rawJson.joinToString(",") + "}"
+    }
 }
