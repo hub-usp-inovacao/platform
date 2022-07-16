c4b87fd727166535bb6f6be23262ff7e11098c77
João Daniel <jotaf.daniel@gmail.com>
2022-07-16 12:28:11 -0300

    add simple help message to hub-cli root command

--- server/hub-cli/src/main/kotlin/br/usp/inovacao/hubusp/server/cli/CLI.kt
+++ server/hub-cli/src/main/kotlin/br/usp/inovacao/hubusp/server/cli/CLI.kt
@@ -2,6 +2,8 @@ package br.usp.inovacao.hubusp.server.cli
 
 import com.github.ajalt.clikt.core.CliktCommand
 
-class CLI : CliktCommand() {
-    override fun run() {}
+class CLI : CliktCommand(help = "The Hub CLI tool to assist automation of platform's tasks") {
+    override fun run() {
+        println(this.shortHelp())
+    }
 }
