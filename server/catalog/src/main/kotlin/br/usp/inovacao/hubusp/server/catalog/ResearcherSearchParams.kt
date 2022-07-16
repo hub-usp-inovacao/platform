ac722f91a92823ffd33ed62b84ad2c95abe9e496
João Daniel <jotaf.daniel@gmail.com>
2022-07-16 17:03:40 -0300

    add Researcher (aka Skill) in Catalog

---
+++ server/catalog/src/main/kotlin/br/usp/inovacao/hubusp/server/catalog/ResearcherSearchParams.kt
@@ -0,0 +1,9 @@
+package br.usp.inovacao.hubusp.server.catalog
+
+data class ResearcherSearchParams(
+    val majorArea: Set<String> = emptySet(),
+    val minorArea: Set<String> = emptySet(),
+    val campus: String? = null,
+    val unity: String? = null,
+    val bond: String? = null
+)
