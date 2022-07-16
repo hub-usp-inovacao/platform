c79382b6be48df390ef8a285eb915830930ff479
João Daniel <jotaf.daniel@gmail.com>
2022-07-16 15:26:46 -0300

    test CatalogDisciplineRepositoryImpl

---
+++ server/persistence/src/test/kotlin/br/usp/inovacao/hubusp/server/persistence/CatalogDisciplineRepositoryImplTest.kt
@@ -0,0 +1,177 @@
+package br.usp.inovacao.hubusp.server.persistence
+
+import br.usp.inovacao.hubusp.server.catalog.Category
+import br.usp.inovacao.hubusp.server.catalog.Discipline
+import com.mongodb.client.MongoDatabase
+import org.litote.kmongo.deleteMany
+import org.litote.kmongo.getCollection
+import kotlin.test.AfterTest
+import kotlin.test.BeforeTest
+import kotlin.test.Test
+import kotlin.test.assertIs
+import kotlin.test.assertTrue
+
+internal class CatalogDisciplineRepositoryImplTest {
+    private lateinit var testDb: MongoDatabase
+    private lateinit var underTest: CatalogDisciplineRepositoryImpl
+
+    @BeforeTest
+    fun setup() {
+        testDb = connectToTestDb()
+        underTest = CatalogDisciplineRepositoryImpl(testDb)
+        seedTestDb()
+    }
+
+    @AfterTest
+    fun teardown() {
+        cleanTestDb()
+    }
+
+    @Test
+    fun `it works when filter is empty`() {
+        // given
+        val params = emptyMap<String, List<String>>()
+
+        // when
+        val result = underTest.filter(params)
+
+        // then
+        assertIs<Set<Discipline>>(result)
+    }
+
+    @Test
+    fun `it filters by campus`() {
+        // given
+        val givenCampus = "São Carlos"
+        val params = mapOf("campus" to listOf(givenCampus))
+
+        // when
+        val result = underTest.filter(params)
+
+        // then
+        assertTrue { result.all { it.campus == givenCampus } }
+    }
+
+    @Test
+    fun `it filters by unity`() {
+        // given
+        val givenUnity = "BCD"
+        val params = mapOf("unity" to listOf(givenUnity))
+
+        // when
+        val result = underTest.filter(params)
+
+        // then
+        assertTrue { result.all { it.unity == givenUnity } }
+    }
+
+    @Test
+    fun `it filters by level`() {
+        // given
+        val givenLevel = "Tenho uma ideia, e agora?"
+        val params = mapOf("level" to listOf(givenLevel))
+
+        // when
+        val result = underTest.filter(params)
+
+        // then
+        assertTrue { result.all { it.level == givenLevel } }
+    }
+
+    @Test
+    fun `it filters by nature`() {
+        // given
+        val givenNature = "Graduação"
+        val params = mapOf("nature" to listOf(givenNature))
+
+        // when
+        val result = underTest.filter(params)
+
+        // then
+        assertTrue { result.all { it.nature == givenNature } }
+    }
+
+    @Test
+    fun `it filters by category`() {
+        // given
+        val params = mapOf("categories" to listOf("Negócios,Empreendedorismo"))
+
+        // when
+        val result = underTest.filter(params)
+
+        // then
+        assertTrue { result.all { it.category.business || it.category.entrepreneurship } }
+    }
+
+    private fun connectToTestDb() = configureDB(
+        protocol = System.getenv("HUB_TEST_DATASOURCE_PROTOCOL") ?: "mongodb",
+        host = System.getenv("HUB_TEST_DATASOURCE_HOST") ?: "localhost",
+        port = System.getenv("HUB_TEST_DATASOURCE_PORT") ?: "27017",
+        dbName = System.getenv("HUB_TEST_DATASOURCE_DBNAME") ?: "test_database"
+    )
+
+    private fun seedTestDb() {
+        val disciplineCollection = testDb.getCollection<Discipline>("disciplines")
+        disciplineCollection.insertMany(testSeeds())
+    }
+
+    private fun cleanTestDb() {
+        val disciplineCollection = testDb.getCollection<Discipline>("disciplines")
+        disciplineCollection.deleteMany("{}")
+    }
+
+    private fun testSeeds() = listOf(
+        Discipline(
+            name = "ABC0001",
+            unity = "ABC",
+            campus = "Butantã",
+            level = "Quero aprender!",
+            nature = "Graduação",
+            category = Category(
+                innovation = false,
+                entrepreneurship = false,
+                business = false,
+                intellectual_property = true
+            )
+        ),
+        Discipline(
+            name = "ABC0004",
+            unity = "ABC",
+            campus = "Butantã",
+            level = "Quero aprender!",
+            nature = "Pós-graduação",
+            category = Category(
+                innovation = false,
+                entrepreneurship = true,
+                business = false,
+                intellectual_property = true
+            )
+        ),
+        Discipline(
+            name = "ABC0002",
+            unity = "ABC",
+            campus = "Butantã",
+            level = "Quero aprender!",
+            nature = "Pós-graduação",
+            category = Category(
+                innovation = false,
+                entrepreneurship = false,
+                business = false,
+                intellectual_property = true
+            )
+        ),
+        Discipline(
+            name = "ABC0003",
+            unity = "BCD",
+            campus = "São Carlos",
+            level = "Tenho uma ideia, e agora?",
+            nature = "Pós-graduação",
+            category = Category(
+                innovation = false,
+                entrepreneurship = false,
+                business = true,
+                intellectual_property = true
+            )
+        )
+    )
+}
