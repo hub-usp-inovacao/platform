a1d01ab6ffd33166b43aa25022641aeb072edaf5
João Daniel <jotaf.daniel@gmail.com>
2022-07-17 16:27:11 -0300

    add Initiative to Catalog

---
+++ server/catalog/src/main/kotlin/br/usp/inovacao/hubusp/server/catalog/Initiative.kt
@@ -0,0 +1,20 @@
+package br.usp.inovacao.hubusp.server.catalog
+
+@kotlinx.serialization.Serializable
+data class Initiative(
+    val classification: String,
+    val contact: Contact,
+    val description: String,
+    val email: String? = null,
+    val localization: String,
+    val name: String,
+    val tags: Set<String>,
+    val unity: String,
+    val url: String,
+)
+
+@kotlinx.serialization.Serializable
+data class Contact(
+    val info: String,
+    val person: String
+)
---
+++ server/catalog/src/main/kotlin/br/usp/inovacao/hubusp/server/catalog/InitiativeSearchParams.kt
@@ -0,0 +1,6 @@
+package br.usp.inovacao.hubusp.server.catalog
+
+data class InitiativeSearchParams(
+    val classifications: Set<String> = emptySet(),
+    val campus: String? = null
+)
