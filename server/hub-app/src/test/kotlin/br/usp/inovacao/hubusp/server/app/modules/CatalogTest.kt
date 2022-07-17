5ba5316ce38f5ce48812ea07959dd69abd59708b
João Daniel <jotaf.daniel@gmail.com>
2022-07-17 01:26:15 -0300

    expose GET /skills

--- server/hub-app/src/test/kotlin/br/usp/inovacao/hubusp/server/app/modules/CatalogTest.kt
+++ server/hub-app/src/test/kotlin/br/usp/inovacao/hubusp/server/app/modules/CatalogTest.kt
@@ -1,19 +1,42 @@
 package br.usp.inovacao.hubusp.server.app.modules
 
+import br.usp.inovacao.hubusp.server.catalog.Discipline
+import br.usp.inovacao.hubusp.server.catalog.Researcher
 import io.ktor.client.request.get
 import io.ktor.client.statement.bodyAsText
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.testing.ApplicationTestBuilder
 import io.ktor.server.testing.testApplication
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
 import kotlin.test.Test
+import kotlin.test.assertContentEquals
 import kotlin.test.assertEquals
 
 class CatalogTest {
     @Test
     fun testGetDisciplines() = testCatalogApplication {
+        // given ... when
         val response = client.get("/disciplines")
+        val body = response.bodyAsText().let {
+            Json.decodeFromString<Map<String, List<Discipline>>>(it)
+        }
 
-        println(response.bodyAsText())
+        // then
+        assertContentEquals(listOf("disciplines"), body.keys.toList())
+        assertEquals(HttpStatusCode.OK, response.status)
+    }
+
+    @Test
+    fun testGetResearchers() = testCatalogApplication {
+        // given ... when
+        val response = client.get("/skills")
+        val body = response.bodyAsText().let {
+            Json.decodeFromString<Map<String, List<Researcher>>>(it)
+        }
+
+        // then
+        assertContentEquals(listOf("skills"), body.keys.toList())
         assertEquals(HttpStatusCode.OK, response.status)
     }
 
