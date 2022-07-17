1920df43952aaebc7498551f0d7f674c56f7a739
João Daniel <jotaf.daniel@gmail.com>
2022-07-17 17:41:25 -0300

    add Initiative to Catalog module

--- frontend/modules/Catalog/pages/iniciativas.vue
+++ frontend/modules/Catalog/pages/iniciativas.vue
@@ -8,7 +8,6 @@
         forms-call="Confira os Editais"
         second-url="http://www.inovacao.usp.br/programas/"
         second-call="confira os Programas"
-        :value="preSearch"
         @search="search.term = $event"
         @clear="search.iniciatives = undefined"
       />
@@ -17,7 +16,6 @@
     <Background class="absolute" />
 
     <MultipleFilters
-      :pre-selected-tabs="preSelectedTabs"
       :tabs="tabs"
       :colors="{ base: '#222c63', active: '#525c93' }"
       :groups="groups"
@@ -25,9 +23,8 @@
     />
 
     <DisplayData
-      :items="display_entries"
+      :items="initiatives"
       group-name="Iniciativas"
-      :selected="preSelected"
       :has-image="false"
     >
       <template #title="{ item }">{{ item.name }}</template>
@@ -39,9 +36,7 @@
             <p class="body-2 mb-4 mr-8">{{ item.email }}</p>
           </v-col>
           <v-col>
-            <p
-              class="body-2 mb-4"
-            >
+            <p class="body-2 mb-4">
               {{ item.contact.info }}
             </p>
           </v-col>
@@ -63,13 +58,12 @@
 </template>
 
 <script>
-import { mapActions, mapGetters } from "vuex";
-import { removeAccent } from "@/lib/format";
+import { debounce } from "debounce";
 
-import Background from "@/components/first_level/Background.vue";
-import Panel from "@/components/first_level/Panel.vue";
-import MultipleFilters from "@/components/first_level/MultipleFilters.vue";
-import DisplayData from "@/components/first_level/DisplayData.vue";
+import Background from "../components/Background.vue";
+import DisplayData from "../components/DisplayData.vue";
+import MultipleFilters from "../components/MultipleFilters.vue";
+import Panel from "../components/Panel.vue";
 
 export default {
   components: {
@@ -127,17 +121,9 @@ export default {
     },
 
     filters: undefined,
-    filtered: undefined,
-    queryParam: undefined,
-    routeParam: undefined,
+    initiatives: [],
   }),
   computed: {
-    ...mapGetters({
-      iniciatives: "iniciativas/iniciatives",
-      isEmpty: "iniciativas/isEmpty",
-      dataStatus: "iniciativas/dataStatus",
-      searchKeys: "iniciativas/searchKeys",
-    }),
     groups() {
       return [
         {
@@ -146,112 +132,22 @@ export default {
             .map((c) => c.name)
             .concat(["Toda a USP"])
             .sort(),
-          preSelected: this.queryParam ? this.queryParam.campus : undefined,
         },
       ];
     },
-    searchTerm() {
-      return this.search.term;
-    },
-    baseItems() {
-      return this.filtered !== undefined ? this.filtered : this.iniciatives;
-    },
-    display_entries() {
-      return this.search.iniciatives !== undefined
-        ? this.search.iniciatives
-        : this.baseItems;
-    },
-    preSelected() {
-      if (this.queryParam && this.queryParam.nome) {
-        return this.display_entries.find(
-          (item) => item.name == this.queryParam.nome
-        );
-      }
-
-      return this.routeParam;
-    },
-    preSearch() {
-      return this.queryParam ? this.queryParam.buscar : undefined;
-    },
-    preSelectedTabs() {
-      if (this.queryParam && this.queryParam.areas) {
-        return this.queryParam.areas
-          .split(";")
-          .map((area) => area.trim())
-          .filter((area) => area.trim().length > 0);
-      }
-
-      return undefined;
-    },
   },
   watch: {
-    isEmpty() {
-      if (!this.isEmpty) {
-        if (this.filters != undefined || this.searchTerm != "") {
-          this.pipeline();
-        }
-      }
-    },
-    searchTerm() {
-      console.log(this.search.term);
-      this.pipeline();
-    },
-    filters() {
-      this.pipeline();
-    },
+    filters: debounce(async function () {
+      const params = {
+        classifications: this.filters.primary,
+        campus: this.filters.terciary[0],
+      };
+
+      this.initiatives = await this.$InitiativeAdapter.filterData(params);
+    }, 1000),
   },
-  beforeMount() {
-    const env = { sheetsAPIKey: process.env.sheetsAPIKey };
-    const route = this.$route;
-
-    if (this.dataStatus == "ok" && this.iniciatives.length == 0) {
-      this.fetchSpreadsheets(env);
-    }
-
-    if (route.params.id) {
-      this.routeParam = this.iniciatives.find(
-        (iniciative) => iniciative.id == route.params.id
-      );
-    } else if (route.query && Object.keys(route.query).length > 0) {
-      this.queryParam = route.query;
-    }
-
-    if (this.queryParam && this.queryParam.buscar) {
-      this.search.term = this.queryParam.buscar;
-    }
-  },
-  methods: {
-    ...mapActions({
-      fetchSpreadsheets: "iniciativas/fetchSpreadsheets",
-      setStrictResults: "global/setStrictResults",
-      setFlexibleResults: "global/setFlexibleResults",
-    }),
-    fuzzySearch() {
-      const searchResult = this.$fuzzySearch(
-        removeAccent(this.search.term.trim()),
-        this.baseItems,
-        this.searchKeys
-      );
-
-      this.search.iniciatives = searchResult;
-    },
-    filterData(context) {
-      this.filtered = this.iniciatives.filter((iniciative) =>
-        this.$iniciativeMatchesFilter(iniciative,context)
-      );
-    },
-    async pipeline() {
-      if (this.filters) this.filterData(this.filters);
-
-      if (this.search.term && this.search.term.trim()) {
-        this.$ga.event({
-          eventCategory: "Iniciativas",
-          eventAction: "Search",
-          eventLabel: this.search.term,
-        });
-        this.fuzzySearch();
-      }
-    },
+  async beforeMount() {
+    this.initiatives = await this.$InitiativeAdapter.requestData();
   },
 };
 </script>
