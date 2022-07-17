c081c0178827c47daa56980438ed27ad3afb8bd5
João Daniel <jotaf.daniel@gmail.com>
2022-07-17 12:57:39 -0300

    add Researcher to Catalog module

---
+++ frontend/modules/Catalog/plugins/ResearcherAdapter.js
@@ -0,0 +1,42 @@
+function ResearcherAdapter(axios) {
+  const baseURL = "http://localhost:8080";
+
+  async function requestData() {
+    const url = baseURL + "/skills";
+
+    try {
+      const { skills } = await axios.$get(url);
+      return skills;
+    } catch (error) {
+      return [];
+    }
+  }
+
+  async function filterData(prms) {
+    const url = baseURL + "/skills";
+
+    const params = Object.assign({}, prms);
+
+    if (params.areaMajors?.length > 0) {
+      params.areaMajors = params.areaMajors.join(",");
+    }
+
+    if (params.areaMinors?.length > 0) {
+      params.areaMinors = params.areaMinors.join(",");
+    }
+
+    try {
+      const { skills } = await axios.$get(url, { params });
+      return skills;
+    } catch (error) {
+      return [];
+    }
+  }
+
+  return { requestData, filterData };
+}
+
+export default (context, inject) => {
+  const adapter = ResearcherAdapter(context.$axios);
+  inject("ResearcherAdapter", adapter);
+};
