97fac86c512a94eb9101c3fe736a00ac0714f35d
João Daniel <jotaf.daniel@gmail.com>
2022-07-17 02:01:19 -0300

    add Company to Catalog model

---
+++ server/catalog/src/main/kotlin/br/usp/inovacao/hubusp/server/catalog/Company.kt
@@ -0,0 +1,34 @@
+package br.usp.inovacao.hubusp.server.catalog
+
+@kotlinx.serialization.Serializable
+data class Company(
+    val address: Address,
+    val classification: Classification,
+    val cnae: String,
+    val companySize: Set<String>,
+    val description: String,
+    val ecosystems: Set<String>,
+    val emails: Set<String>,
+    val incubated: String,
+    val logo: String? = null,
+    val name: String,
+    val phones: Set<String>,
+    val services: Set<String>,
+    val technologies: Set<String>,
+    val url: String
+)
+
+@kotlinx.serialization.Serializable
+data class Classification(
+    val major: String,
+    val minor: String
+)
+
+@kotlinx.serialization.Serializable
+data class Address(
+    val cep: String,
+    val city: Set<String>,
+    val neighborhood: String,
+    val state: String,
+    val venue: String
+)
