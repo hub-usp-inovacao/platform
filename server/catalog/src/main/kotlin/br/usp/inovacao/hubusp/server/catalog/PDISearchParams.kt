f6f95886b572f2bc495de3009e6ccb3a8f8e9992
João Daniel <jotaf.daniel@gmail.com>
2022-08-05 16:02:34 -0300

    implement text-based search for PDI
    Co-authored-by: Briza Mel <brizamel.dias@gmail.com>
    Co-authored-by: João Daniel <jotaf.daniel@gmail.com>
    Co-authored-by: Luiz Carlos <luizcarlosdk@hotmail.com>
    Co-authored-by: Marilia Dicezare <mariliatd@gmail.com>

--- server/catalog/src/main/kotlin/br/usp/inovacao/hubusp/server/catalog/PDISearchParams.kt
+++ server/catalog/src/main/kotlin/br/usp/inovacao/hubusp/server/catalog/PDISearchParams.kt
@@ -3,4 +3,5 @@ package br.usp.inovacao.hubusp.server.catalog
 data class PDISearchParams(
     val categories: Set<String> = emptySet(),
     val campus: String? = null,
+    val term: String? = null,
 )
