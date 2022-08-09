972193eb9318c29a75f63decb0b371b9b36b97af
BrizaMel <brizamel.dias@gmail.com>
2022-08-09 17:21:13 -0300

    Implement initiatives text-based search
    Signed-off-by: BrizaMel <brizamel.dias@gmail.com>

--- server/persistence/src/test/kotlin/br/usp/inovacao/hubusp/server/persistence/InitiativeExtensionKtTest.kt
+++ server/persistence/src/test/kotlin/br/usp/inovacao/hubusp/server/persistence/InitiativeExtensionKtTest.kt
@@ -1,6 +1,7 @@
 package br.usp.inovacao.hubusp.server.persistence
 
 import br.usp.inovacao.hubusp.server.catalog.InitiativeSearchParams
+import br.usp.inovacao.hubusp.server.catalog.PDISearchParams
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
@@ -45,6 +46,41 @@ internal class InitiativeExtensionKtTest {
 
         // then
         val expected = "{\"localization\":\"$campus\"}"
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun `it parses text term`() {
+        // given
+        val term = "inovação"
+        val params = InitiativeSearchParams(term = term)
+
+        // when
+        val result = params.toCollectionFilter()
+
+        // then
+        val expected = "{\"\$text\":{\"\$search\":\"$term\"}}"
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun `it parses all fields`() {
+        // given
+        val classification1 = "Agente Institucional"
+        val classification2 = "Entidade Estudantil"
+        val campus = "Butantã"
+        val term = "inovação"
+        val params = InitiativeSearchParams(
+            classifications = setOf(classification1, classification2),
+            campus = campus,
+            term = term
+        )
+
+        // when
+        val result = params.toCollectionFilter()
+
+        // then
+        val expected = "{\"\$or\":[{\"classification\":\"$classification1\"},{\"classification\":\"$classification2\"}],\"localization\":\"$campus\",\"\$text\":{\"\$search\":\"$term\"}}"
         assertEquals(expected, result)
     }
 }
