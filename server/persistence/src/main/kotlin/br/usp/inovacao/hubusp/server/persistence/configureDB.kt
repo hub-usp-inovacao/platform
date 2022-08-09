0ffa18f4ac46d3a23e4a97f796c8715f2a3768ad
mariliatd <mariliatd@gmail.com>
2022-08-09 17:21:29 -0300

    Add text-based search for patents
    Signed-off-by: mariliatd <mariliatd@gmail.com>

--- server/persistence/src/main/kotlin/br/usp/inovacao/hubusp/server/persistence/configureDB.kt
+++ server/persistence/src/main/kotlin/br/usp/inovacao/hubusp/server/persistence/configureDB.kt
@@ -2,6 +2,7 @@ package br.usp.inovacao.hubusp.server.persistence
 
 import br.usp.inovacao.hubusp.server.catalog.Company
 import br.usp.inovacao.hubusp.server.catalog.PDI
+import br.usp.inovacao.hubusp.server.catalog.Patent
 import com.mongodb.MongoCommandException
 import com.mongodb.client.MongoDatabase
 import org.litote.kmongo.KMongo
@@ -27,6 +28,9 @@ fun configureDB(protocol: String, host: String, port: String, dbName: String): M
 
     db.getCollection("skills")
         .createIndex("""{"name":"text","skills":"text","equipments":"text","services":"text","keywords":"text"}""")
+
+    db.getCollection<Patent>("patents")
+        .createIndex("""{"name":"text","summary":"text","owners":"text","inventors":"text"}""")
 
     return db
 }
