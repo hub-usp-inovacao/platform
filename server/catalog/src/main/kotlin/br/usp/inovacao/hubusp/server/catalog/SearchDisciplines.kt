dc02aa85d6ab9bcbb6beab27a3dc68b1520307cd
João Daniel <jotaf.daniel@gmail.com>
2022-08-09 18:32:17 -0300

    remove old structures
    Signed-off-by: João Daniel <jotaf.daniel@gmail.com>

--- server/catalog/src/main/kotlin/br/usp/inovacao/hubusp/server/catalog/SearchDisciplines.kt
+++ server/catalog/src/main/kotlin/br/usp/inovacao/hubusp/server/catalog/SearchDisciplines.kt
@@ -3,10 +3,6 @@ package br.usp.inovacao.hubusp.server.catalog
 class SearchDisciplines(
     private val disciplineRepository: DisciplineRepository
 ) {
-    fun search(params: Map<String, List<String>>): Set<Discipline> {
-        return disciplineRepository.filter(params)
-    }
-
     fun search(params: DisciplineSearchParams): Set<Discipline> {
         return disciplineRepository.filter(params)
     }
