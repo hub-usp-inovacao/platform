b7a145164cf9d2975cac0eacc60f70722607e14d
João Daniel <jotaf.daniel@gmail.com>
2022-08-23 15:59:45 -0300

    create initial version of NDText component
    Co-authored-by: João Daniel <jotaf.daniel@gmail.com>
    Co-authored-by: Luiz Carlos <luizcarlosdk@hotmail.com>
    Co-authored-by: Marilia Dicezare <mariliatd@gmail.com>

---
+++ frontend/modules/Catalog/components/NDText.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <p v-if="text !== 'N/D'" :class="classes">{{ text }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    text: {
+      type: String,
+      default: "",
+    },
+    classes: {
+      type: String,
+      default: "",
+    },
+  },
+};
+</script>
