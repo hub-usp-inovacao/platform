189fd80bf09ea6962e5b72de87d45c1ef76b4aef
João Daniel <jotaf.daniel@gmail.com>
2022-09-05 15:49:43 -0300

    extract mailer to its own gradle project

--- server/curatorship/build.gradle.kts
+++ server/curatorship/build.gradle.kts
@@ -4,7 +4,6 @@ val logback_version: String by project
 val mockk_version: String by project
 val ktor_version : String by project
 val typesafe_config_version: String by project
-val javax_mail_version: String by project
 
 plugins {
     kotlin("jvm")
@@ -21,6 +20,8 @@ repositories {
 }
 
 dependencies {
+    implementation(project(":mailer"))
+
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:$kotlinx_serialization_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
     implementation("io.ktor:ktor-client-core:$ktor_version")
@@ -28,7 +29,6 @@ dependencies {
     implementation("io.ktor:ktor-client-content-negotiation:$ktor_version")
     implementation("io.ktor:ktor-serialization-kotlinx-json:$ktor_version")
     implementation("com.typesafe:config:$typesafe_config_version")
-    implementation("javax.mail:mail:$javax_mail_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
     testImplementation("io.mockk:mockk:$mockk_version")
 }
--- server/curatorship/src/main/kotlin/br/usp/inovacao/hubusp/curatorship/Mailer.kt
+++ server/curatorship/src/main/kotlin/br/usp/inovacao/hubusp/curatorship/Mailer.kt
@@ -1,83 +1,15 @@
 package br.usp.inovacao.hubusp.curatorship
 
-import java.util.Properties
-import javax.mail.Authenticator
-import javax.mail.PasswordAuthentication
-import javax.mail.Session
-import javax.mail.internet.InternetAddress
-import javax.mail.internet.MimeMessage
-
-private class Mail(
-    val to: List<String>,
-    subject: String,
-    val body: String,
-    val ccs: List<String> = emptyList(),
-) {
-    val subject: String
-
-    init {
-        this.subject = formatSubject(subject)
-    }
-
-    private fun formatSubject(subject: String) = "[HubUSPInovação] - $subject"
-}
-
-private class MailerImpl(
-    private val user: String,
-    private val password: String
-) {
-    private val protocol = "smtp"
-    private val host = "smtp.gmail.com"
-
-    private val auth = object : Authenticator() {
-        override fun getPasswordAuthentication() =
-            PasswordAuthentication(user, password)
-    }
-
-    private val config = Properties()
-
-    init {
-        with(config) {
-            put("mail.smtp.host", host)
-            put("mail.smtp.port", "587")
-            put("mail.smtp.auth", true)
-            put("mail.smtp.starttls.enable", true)
-            put("mail.smtp.ssl.protocols", "TLSv1.2")
-        }
-    }
-
-    private fun buildMessage(session: Session, mail: Mail): MimeMessage {
-        val message = MimeMessage(session)
-        with(message) {
-            setFrom(user)
-            mail.to.forEach { addRecipient(javax.mail.Message.RecipientType.TO, InternetAddress(it)) }
-            mail.ccs.forEach { addRecipient(javax.mail.Message.RecipientType.BCC, InternetAddress(it)) }
-            subject = mail.subject
-            setText(mail.body)
-        }
-        return message
-    }
-
-    fun send(mail: Mail) {
-        val session = Session.getInstance(config, auth)
-        val message = buildMessage(session, mail)
-        val transport = session.getTransport(protocol)
-        with(transport) {
-            connect(host, user, password)
-            sendMessage(message, message.allRecipients)
-            close()
-        }
-    }
-}
+import br.usp.inovacao.hubusp.mailer.Mail
 
 class Mailer(
     user: String,
     password: String
 ) {
-    private val mailerImpl: MailerImpl
+    private val mailerImpl: br.usp.inovacao.hubusp.mailer.Mailer
 
     init {
-        mailerImpl = MailerImpl(user, password)
+        mailerImpl = br.usp.inovacao.hubusp.mailer.Mailer(user, password)
     }
 
     fun notifySpreadsheetError(message: String) {
