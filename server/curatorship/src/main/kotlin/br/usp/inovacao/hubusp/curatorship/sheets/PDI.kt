a0713f39c1475b3242ba64e9412972c9d648086e
mariliatd <mariliatd@gmail.com>
2022-09-09 17:43:49 -0300

    Begin implementation of RefreshPDI
    Co-authored-by: Briza Mel <brizamel.dias@gmail.com>
    Co-authored-by: João Daniel <jotaf.daniel@gmail.com>
    Co-authored-by: Luiz Carlos <luizcarlosdk@hotmail.com>
    Co-authored-by: Marilia Dicezare <mariliatd@gmail.com>

---
+++ server/curatorship/src/main/kotlin/br/usp/inovacao/hubusp/curatorship/sheets/PDI.kt
@@ -0,0 +1,37 @@
+package br.usp.inovacao.hubusp.curatorship.sheets
+
+@kotlinx.serialization.Serializable
+data class PDI(
+    val category: PDICategory,
+    val name: String
+) {
+    companion object {
+        fun from(category: String, name: String): PDI {
+            ensureNameIsNotEmpty(name)
+            val pdiCategory = PDICategory.from(category)
+            return PDI(category = pdiCategory, name = name)
+        }
+
+        private fun ensureNameIsNotEmpty(name: String) {
+            if (name.isEmpty()) throw ValidationException("Name cannot be empty")
+        }
+    }
+}
+
+enum class PDICategory {
+    CEPID, EMBRAPII, INCT, NAP, EngineeringCenter;
+
+    companion object {
+        fun from(value: String) = when (value) {
+            "CEPID" -> CEPID
+            "EMBRAPII" -> EMBRAPII
+            "INCT" -> INCT
+            "NAP" -> NAP
+            "Centro de Pesquisa em Engenharia" -> EngineeringCenter
+
+            else -> throw ValidationException("$value is not accepted as PDICategory")
+        }
+    }
+}
+
+class ValidationException(msg: String) : RuntimeException(msg)
