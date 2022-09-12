547172301e38711d344d27f6c7f84744f0e25644
João Daniel <jotaf.daniel@gmail.com>
2022-09-12 09:53:22 -0300

    create InitiativeModel defining the document in DB
    - it has a parser to Catalog::Initiative

--- server/persistence/src/main/kotlin/br/usp/inovacao/hubusp/server/persistence/CatalogInitiativeRepositoryImpl.kt
+++ server/persistence/src/main/kotlin/br/usp/inovacao/hubusp/server/persistence/CatalogInitiativeRepositoryImpl.kt
@@ -1,20 +1,43 @@
 package br.usp.inovacao.hubusp.server.persistence
 
+import br.usp.inovacao.hubusp.server.catalog.Contact
 import br.usp.inovacao.hubusp.server.catalog.Initiative
 import br.usp.inovacao.hubusp.server.catalog.InitiativeRepository
 import br.usp.inovacao.hubusp.server.catalog.InitiativeSearchParams
+import br.usp.inovacao.hubusp.server.persistence.models.InitiativeContact
+import br.usp.inovacao.hubusp.server.persistence.models.InitiativeModel
 import com.mongodb.client.MongoDatabase
 import org.litote.kmongo.find
 import org.litote.kmongo.getCollection
 
+fun InitiativeContact.toCatalogContact(): Contact = Contact(
+    info = this.info,
+    person = this.person
+)
+
+fun InitiativeModel.toCatalogInitiative(): Initiative = Initiative(
+    classification = this.classification,
+    contact = this.contact.toCatalogContact(),
+    description = this.description,
+    email = this.email,
+    localization = this.localization,
+    name = this.name,
+    tags = this.tags,
+    unity = this.unity,
+    url = this.url
+)
+
 class CatalogInitiativeRepositoryImpl(
     db: MongoDatabase
 ): InitiativeRepository {
-    private val initiativeCollection = db.getCollection<Initiative>("iniciatives")
+    private val initiativeCollection = db.getCollection<InitiativeModel>("iniciatives")
 
     override fun filter(params: InitiativeSearchParams): Set<Initiative> {
         val filter = params.toCollectionFilter()
 
-        return initiativeCollection.find(filter).toSet()
+        return initiativeCollection
+            .find(filter)
+            .map { it.toCatalogInitiative() }
+            .toSet()
     }
 }
---
+++ server/persistence/src/main/kotlin/br/usp/inovacao/hubusp/server/persistence/models/InitiativeModel.kt
@@ -0,0 +1,22 @@
+package br.usp.inovacao.hubusp.server.persistence.models
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class InitiativeModel(
+    val classification: String,
+    val contact: InitiativeContact,
+    val description: String,
+    val email: String? = null,
+    val localization: String,
+    val name: String,
+    val tags: Set<String>,
+    val unity: String,
+    val url: String,
+)
+
+@Serializable
+data class InitiativeContact(
+    val info: String,
+    val person: String
+)
