25075a8d20cd0ea3c5989a8b2151b96a06e98e67
João Daniel <jotaf.daniel@gmail.com>
2022-09-14 10:56:24 -0300

    add new metrics to kotlin and grafana dashboard
    Signed-off-by: João Daniel <jotaf.daniel@gmail.com>

--- server/hub-app/src/main/kotlin/br/usp/inovacao/hubusp/server/app/ConfigureMetrics.kt
+++ server/hub-app/src/main/kotlin/br/usp/inovacao/hubusp/server/app/ConfigureMetrics.kt
@@ -4,6 +4,9 @@ import io.ktor.server.application.*
 import io.ktor.server.metrics.micrometer.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import io.micrometer.core.instrument.binder.jvm.JvmGcMetrics
+import io.micrometer.core.instrument.binder.jvm.JvmMemoryMetrics
+import io.micrometer.core.instrument.binder.system.ProcessorMetrics
 import io.micrometer.prometheus.PrometheusConfig
 import io.micrometer.prometheus.PrometheusMeterRegistry
 
@@ -11,6 +14,11 @@ fun Application.configureMetrics() {
     val appMicrometerRegistry = PrometheusMeterRegistry(PrometheusConfig.DEFAULT)
     install(MicrometerMetrics) {
         registry = appMicrometerRegistry
+        meterBinders = listOf(
+            JvmMemoryMetrics(),
+            JvmGcMetrics(),
+            ProcessorMetrics()
+        )
     }
 
     routing {
