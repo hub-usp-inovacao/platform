990fcfc5228dd053dcb126d5c295eaf624593b2e
João Daniel <jotaf.daniel@gmail.com>
2022-09-15 20:21:09 -0300

    implement missing repository methods

--- server/persistence/build.gradle.kts
+++ server/persistence/build.gradle.kts
@@ -3,6 +3,9 @@ val kotlin_version: String by project
 val logback_version: String by project
 val mockk_version: String by project
 val kmongo_version: String by project
+val ktor_version: String by project
+val typesafe_config_version: String by project
+val coroutines_version: String by project
 
 plugins {
     kotlin("jvm")
@@ -23,6 +26,12 @@ dependencies {
     implementation(project(":curatorship"))
     implementation(project(":discovery"))
 
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutines_version")
+    implementation("com.typesafe:config:$typesafe_config_version")
+    implementation("io.ktor:ktor-client-core:$ktor_version")
+    implementation("io.ktor:ktor-client-cio:$ktor_version")
+    implementation("io.ktor:ktor-client-content-negotiation:$ktor_version")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktor_version")
     implementation("org.litote.kmongo:kmongo-serialization:$kmongo_version")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:$kotlinx_serialization_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
--- server/persistence/src/main/kotlin/br/usp/inovacao/hubusp/server/persistence/journey/JourneyRepositoryImpl.kt
+++ server/persistence/src/main/kotlin/br/usp/inovacao/hubusp/server/persistence/journey/JourneyRepositoryImpl.kt
@@ -1,26 +1,51 @@
 package br.usp.inovacao.hubusp.server.persistence.journey
 
 import br.usp.inovacao.hubusp.server.discovery.*
+import br.usp.inovacao.hubusp.server.persistence.Configuration
 import br.usp.inovacao.hubusp.server.persistence.models.DisciplineModel
 import br.usp.inovacao.hubusp.server.persistence.models.InitiativeModel
 import br.usp.inovacao.hubusp.server.persistence.models.PDIModel
 import com.mongodb.client.MongoCollection
 import com.mongodb.client.MongoDatabase
+import io.ktor.client.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
+import io.ktor.serialization.kotlinx.json.*
+import kotlinx.coroutines.runBlocking
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
 import org.litote.kmongo.find
 import org.litote.kmongo.getCollection
+
+@Serializable
+private data class ValueRange(
+    val range: String,
+    val majorDimension: String,
+    val values: List<List<String>>
+)
 
 class JourneyRepositoryImpl(
     db: MongoDatabase
 ) : JourneyRepository {
 
-    private var disciplineCollection: MongoCollection<DisciplineModel>
-    private var initiativeCollection: MongoCollection<InitiativeModel>
-    private var pdiCollection: MongoCollection<PDIModel>
+    private val disciplineCollection: MongoCollection<DisciplineModel>
+    private val initiativeCollection: MongoCollection<InitiativeModel>
+    private val pdiCollection: MongoCollection<PDIModel>
+    private val httpClient: HttpClient
 
     init {
         disciplineCollection = db.getCollection<DisciplineModel>("disciplines")
         initiativeCollection = db.getCollection<InitiativeModel>("iniciatives")
         pdiCollection = db.getCollection<PDIModel>("pdis")
+        httpClient = HttpClient(CIO) {
+            install(ContentNegotiation) {
+                json()
+            }
+        }
     }
 
     override fun find(step: JourneyStep, filter: Filter) = when(step) {
@@ -55,10 +80,28 @@ class JourneyRepositoryImpl(
             .toSet()
     }
 
+    private fun matchesCreateFilter(filter: CreateStepFilter, row: List<String>): Boolean {
+        if (filter.insideUSP == null) return true
+
+        val insideUSP: Boolean = filter.insideUSP!!
+        val bond = row[1]
+
+        return (insideUSP && bond == "USP") || (!insideUSP && bond == "Outro")
+    }
+
     private fun findCreate(filter: CreateStepFilter): Set<JourneyRecord> {
-        // TODO: fetch incubs from the dedicated spreadsheet
-        // https://docs.google.com/spreadsheets/d/1kS55eqf_xEfOExh1aEGSvjTFDbtCRWHYSEyh4f31nww/edit#gid=591115329
-        return emptySet()
+        val sheetId = Configuration.INCUBATOR_ID
+        val tabName = Configuration.INCUBATOR_TAB
+
+        return readSpreadsheet(sheetId, tabName)
+            .filter { row -> this.matchesCreateFilter(filter, row) }
+            .map {
+                JourneyRecord(
+                    name = it[2],
+                    url = it[3]
+                )
+            }
+            .toSet()
     }
 
     private fun findImprove(filter: ImproveStepFilter): Set<JourneyRecord> {
@@ -73,10 +116,39 @@ class JourneyRepositoryImpl(
             .toSet()
     }
 
-    private fun findFund(filter: Filter): Set<JourneyRecord> {
-        // TODO: fetch funds from the dedicated spreadsheet
-        // https://docs.google.com/spreadsheets/d/12O4u6P5Ytf9_uXmHu89vC_7oE_orw7GDKRajzSzLh5g/edit#gid=0
-        return emptySet()
+    private fun findFund(filter: FundStepFilter): Set<JourneyRecord> {
+        val sheetId = Configuration.FUNDS_ID
+        val tabName = Configuration.FUNDS_TAB
+
+        return readSpreadsheet(sheetId, tabName)
+            .filter { filter.type == null || it[0] == filter.type!! }
+            .map {
+                JourneyRecord(
+                    name = it[1],
+                    url = it[2]
+                )
+            }
+            .toSet()
+    }
+
+    private fun readSpreadsheet(sheetId: String, tabName: String): List<List<String>> = try {
+        val apiKey = Configuration.SHEETS_API_KEY
+        runBlocking {
+            httpClient
+                .get {
+                    url {
+                        protocol = URLProtocol.HTTPS
+                        host = "sheets.googleapis.com"
+                        appendPathSegments("v4", "spreadsheets", sheetId, "values", "'$tabName'")
+                        parameters.append("key", apiKey)
+                    }
+                }
+                .bodyAsText()
+                .let { Json.decodeFromString<ValueRange>(it) }
+                .values
+        }
+    } catch (e: Exception) {
+        emptyList()
     }
 }
 
