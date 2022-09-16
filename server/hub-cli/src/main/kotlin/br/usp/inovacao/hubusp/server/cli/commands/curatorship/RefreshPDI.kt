ff53bf0ac74b7497c21fe3a73186c810e8e580c8
BrizaMel <brizamel.dias@gmail.com>
2022-09-16 18:21:40 -0300

    Implement PDI curatorship persistance
    Signed-off-by: BrizaMel <brizamel.dias@gmail.com>
    Co-authored-by: João Daniel <jotaf.daniel@gmail.com>
    Co-authored-by: Luiz <luizcarlosdk@hotmail.com>
    Co-authored-by: Marília Dicezare <mariliatd@gmail.com>

---
+++ server/hub-cli/src/main/kotlin/br/usp/inovacao/hubusp/server/cli/commands/curatorship/RefreshPDI.kt
@@ -0,0 +1,26 @@
+package br.usp.inovacao.hubusp.server.cli.commands.curatorship
+
+import br.usp.inovacao.hubusp.curatorship.Mailer
+import br.usp.inovacao.hubusp.curatorship.sheets.SpreadsheetReader
+import br.usp.inovacao.hubusp.server.persistence.configureDB
+import br.usp.inovacao.hubusp.server.persistence.curatorship.PDIErrorRepositoryImpl
+import br.usp.inovacao.hubusp.server.persistence.curatorship.PDIRepositoryImpl
+import com.github.ajalt.clikt.core.CliktCommand
+
+class RefreshPDI : CliktCommand() {
+    private val refreshPDI : br.usp.inovacao.hubusp.curatorship.sheets.RefreshPDI
+
+    init {
+        val db = configureDB("", "", "", "")
+        refreshPDI = br.usp.inovacao.hubusp.curatorship.sheets.RefreshPDI(
+            mailer = Mailer("", ""),
+            spreadsheetReader = SpreadsheetReader(""),
+            pdiRepository = PDIRepositoryImpl(db),
+            pdiErrorRepository = PDIErrorRepositoryImpl(db)
+        )
+    }
+
+    override fun run() {
+        refreshPDI.refresh()
+    }
+}
