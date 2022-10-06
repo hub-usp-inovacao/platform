bfcf436664a8314026b182b00d6e4d9b2c26e771
Luiz <luizcarlosdk@hotmail.com>
2022-10-06 08:45:21 -0300

    Create validation among duplicated CNPJs and implement a new test set
    Signed-off-by: Luiz <luizcarlosdk@hotmail.com>

--- backend/spec/models/company_spec.rb
+++ backend/spec/models/company_spec.rb
@@ -269,8 +269,9 @@ RSpec.describe Company, type: :model do
     end
 
     it 'returns invalid company when two companies has the same cnpj' do
-      described_class.create valid_cnpj
-      company = described_class.new valid_cnpj
+      described_class.create valid_attr
+      company = described_class.new valid_attr
+      company[:cnpj] = { value: '12.123.123/0001-21' }
       expect(company).to be_invalid
       described_class.delete_all
     end
@@ -278,6 +279,7 @@ RSpec.describe Company, type: :model do
     it 'returns valid company when two companies has the same name' do
       described_class.create valid_attr
       company = described_class.new valid_attr
+      company[:cnpj] = '13.123.123/0001-21'
       expect(company).to be_valid
       described_class.delete_all
     end
@@ -285,9 +287,9 @@ RSpec.describe Company, type: :model do
     it 'returns valid company when two companies has the same corporate_name' do
       described_class.create valid_attr
       company = described_class.new valid_attr
+      company[:cnpj] = '13.123.123/0001-21'
       expect(company).to be_valid
       described_class.delete_all
     end
-
   end
 end
