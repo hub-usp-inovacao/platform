15624c997014e9c4857e86cd21506f6536e02c67
Luiz <luizcarlosdk@hotmail.com>
2022-12-27 18:12:55 -0300

    avoid to send null selfcreated conexao
    Signed-off-by: Luiz <luizcarlosdk@hotmail.com>

--- backend/app/controllers/conexoes_controller.rb
+++ backend/app/controllers/conexoes_controller.rb
@@ -5,12 +5,12 @@ class ConexoesController < ApplicationController
     @conexao = Conexao.create!(create_params)
     author_email = create_params[:personal][:email]
     ApplicationMailer.conexao_confirmation(author_email).deliver_now
+    render json: { conexao: @conexao }
     new_entries = Conexao.where(delivered: false)
     ApplicationMailer.with(entities: new_entries).conexao.deliver_now
     new_entries.each do |entry|
       entry.update(delivered: true)
     end
-    render json: { conexao: @conexao }
   rescue StandardError => e
     render json: { error: e }, status: :bad_request
   end
