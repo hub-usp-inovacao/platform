5c7721e35660ca36ad30cdfda5c891d4f61d690b
mariliatd <mariliatd@gmail.com>
2023-02-01 16:28:14 -0300

    add long and short descriptions values in Catalog Discipline
    Signed-off-by: mariliatd <mariliatd@gmail.com>

--- server/catalog/src/main/kotlin/br/usp/inovacao/hubusp/server/catalog/Discipline.kt
+++ server/catalog/src/main/kotlin/br/usp/inovacao/hubusp/server/catalog/Discipline.kt
@@ -9,9 +9,16 @@ data class Category(
 )
 
 @kotlinx.serialization.Serializable
+data class Description(
+    val long: String,
+    val short: String,
+)
+
+@kotlinx.serialization.Serializable
 data class Discipline(
     val name: String,
     val category: Category,
+    val description: Description,
     val unity: String,
     val campus: String,
     val level: String,
