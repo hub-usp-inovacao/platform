a8a2ab02076823c8113667031da9eef4f256fd40
José Lucas Silva Mayer <josemayer@usp.br>
2023-03-16 05:32:30 -0300

    Update bar component with position and style fixes
    Signed-off-by: José Lucas Silva Mayer <josemayer@usp.br>

--- frontend/components/UniversalAuspinBar.vue
+++ frontend/components/UniversalAuspinBar.vue
@@ -1,48 +1,85 @@
 <template>
-  <div class="text-center d-flex align-center barra custom-hidden-md-and-down" >
-        <v-btn
-          color="#000428"
-          dark
-          href="http://www.inovacao.usp.br/"
-          target="_blank"
-          class="botao1"
-        >
-          AUSPIN
-        </v-btn>
-
-        <v-btn
-          color="#000428"
-          dark
-          href="https://patente.inovacao.usp.br/inteumweb/inventorportal/"
-          target="_blank"
-          class="botao"
-        >
-          Portal do Inventor
-        </v-btn>
-
-        <v-btn color="#000428" dark href="/" class="botao" > Hub USP Inovação </v-btn>
-      </div>
+  <div>
+    <v-app-bar
+      color="#000428"
+      dark
+      fixed
+      flat
+      :height="barHeight + 'px'"
+      class="custom-hidden-md-and-down bar"
+    >
+      <v-container>
+        <div class="d-inline-flex align-center page-list">
+          <a
+            v-for="item in items"
+            :key="item.title"
+            :href="item.href"
+            target="_blank"
+            :class="{ active: item.href == '/' }"
+          >
+            <div class="page">
+              {{ item.title }}
+            </div>
+          </a>
+        </div>
+        <v-spacer></v-spacer>
+      </v-container>
+    </v-app-bar>
+    <div class="bar filler" :style="{ height: barHeight + 'px' }"></div>
+  </div>
 </template>
 
 <script>
 export default {
-
+  data: () => ({
+    items: [
+      { title: 'AUSPIN', href: 'http://www.inovacao.usp.br/' },
+      {
+        title: 'Portal do Inventor',
+        href: 'https://patente.inovacao.usp.br/inteumweb/inventorportal/'
+      },
+      { title: 'Hub USP Inovação', href: '/' },
+    ]
+  }),
+  computed: {
+    barHeight() {
+      return 50;
+    }
+  },
 }
 </script>
 
 
-<style>
-.barra {
+<style scoped>
+@import url('https://fonts.googleapis.com/css?family=Ubuntu&display=swap');
+
+.bar {
+  border-bottom: 1px solid #fff !important;
+  box-sizing: border-box;
+  box-shadow: 0 0 5px rgba(0,0,0,0.2),
+    0 1px 0 rgba(255,255,255,0.15) !important;
+}
+.filler {
   background-color: #000428;
-  height: 3.1em;
-  justify-content: flex-start;
+  box-shadow: none !important;
 }
-
-.v-btn {
-    text-transform: unset !important;
+.page-list {
+  font-family: 'Ubuntu', 'Arial', sans-serif;
 }
-
-.botao1 {
-  margin-left: 30em;
+.page-list .page {
+  padding: 14px 15px;
+}
+.page-list a {
+  color: rgba(255, 255, 255, 0.7) !important;
+  text-decoration: none !important;
+  transition: color 0.2s ease;
+  font-weight: 400;
+  font-size: 16px;
+}
+.page-list a:hover {
+  color: rgba(255, 255, 255, 1) !important;
+}
+.page-list a.active {
+  color: rgba(255, 255, 255, 1) !important;
 }
 </style>
