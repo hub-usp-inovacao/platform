cb3f38b20a097b26b2aa44680a7c1f458412a24a
José Lucas Silva Mayer <josemayer@usp.br>
2023-04-07 21:11:57 -0300

    Fix container to fit Auspin bar and add search feature
    Signed-off-by: José Lucas Silva Mayer <josemayer@usp.br>

--- frontend/components/UniversalAuspinBar.vue
+++ frontend/components/UniversalAuspinBar.vue
@@ -8,26 +8,62 @@
       :height="barHeight + 'px'"
       class="custom-hidden-md-and-down bar"
     >
-      <div class="d-inline-flex align-center page-list">
-        <a
-          v-for="item in items"
-          :key="item.title"
-          :href="item.href"
-          target="_blank"
-          :class="{ active: item.href == '/' }"
-        >
-          <div class="page">
-            {{ item.title }}
-          </div>
-        </a>
+      <div class="bar-container">
+        <div class="d-inline-flex align-center page-list">
+          <a
+            v-for="item in items"
+            :key="item.title"
+            :href="item.href"
+            target="_blank"
+            :class="{ active: item.href == '/' }"
+          >
+            <div class="page">
+              {{ item.title }}
+            </div>
+          </a>
+        </div>
+        <div class="d-flex">
+          <div class="search-divider"></div>
+          <v-menu
+            rounded="0"
+            offset-y
+            left
+            nudge-left="-4"
+            :close-on-content-click="false"
+          >
+            <template v-slot:activator="{ attrs, on }">
+              <v-btn
+                class="search-icon no-background-hover"
+                icon
+                v-bind="attrs"
+                v-on="on"
+                :ripple="false"
+              >
+                <v-icon class="search">mdi-magnify</v-icon>
+              </v-btn>
+            </template>
+
+            <div class="search-field">
+              <v-text-field
+                placeholder="Pesquise e aperte enter"
+                class="search-input"
+                flat
+                solo
+                dense
+                v-model="search"
+                prepend-icon
+                @keydown.enter="searchQuery"
+              >
+                <template #prepend-inner>
+                  <v-icon class="search" small>mdi-magnify</v-icon>
+                </template>
+              </v-text-field>
+            </div>
+          </v-menu>
+        </div>
       </div>
-      <v-spacer></v-spacer>
-      <v-divider vertical></v-divider>
-      <v-btn class="ml-2" icon>
-        <v-icon class="search">mdi-magnify</v-icon>
-      </v-btn>
     </v-app-bar>
-    <div class="bar filler" :style="{ height: barHeight + 'px' }"></div>
+    <div class="bar filler" :style="{ height: (barHeight + 1) + 'px' }"></div>
   </div>
 </template>
 
@@ -45,9 +81,16 @@ export default {
   }),
   computed: {
     barHeight() {
-      return 50;
+      return 49;
     }
   },
+  methods: {
+    searchQuery() {
+      const encodedQuery = encodeURIComponent(this.search.trim());
+      const searchUrl = `/busca?q=${encodedQuery}`;
+      window.location.href = searchUrl;
+    }
+  }
 }
 </script>
 
@@ -56,21 +99,30 @@ export default {
 @import url('https://fonts.googleapis.com/css?family=Ubuntu&display=swap');
 
 .bar {
-  border-bottom: 1px solid #fff !important;
   box-sizing: border-box;
   box-shadow: 0 0 5px rgba(0,0,0,0.2),
     0 1px 0 rgba(255,255,255,0.15) !important;
-  padding: 0px 60px;
 }
+
+.bar-container {
+  width: 1029px;
+  padding: 0 0px;
+  margin: 0 auto;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
 .filler {
   background-color: #000428;
   box-shadow: none !important;
+  border-bottom: 1px solid white;
 }
 .page-list {
   font-family: 'Ubuntu', 'Arial', sans-serif;
 }
 .page-list .page {
-  padding: 14px 15px;
+  padding: 14px 14px 15px 14px;
 }
 .page-list a {
   color: rgba(255, 255, 255, 0.7) !important;
@@ -84,5 +136,30 @@ export default {
 }
 .page-list a.active {
   color: rgba(255, 255, 255, 1) !important;
+}
+.search-icon {
+  margin: 0 10px 0 5px;
+}
+.search-divider {
+  width: 1px;
+  height: 50px;
+  background-color: rgba(255, 255, 255, 0.10);
+}
+.search-field {
+  width: 340px;
+  height: 68px;
+  background-color: #000428;
+  padding: 15px;
+  color: #fff;
+}
+.search-input {
+  border-radius: 0px !important;
+  font-family: 'Ubuntu', 'Arial', sans-serif;
+  font-size: 14px !important;
+  border: 2px solid #ddd;
+  height: 42px !important;
+}
+.no-background-hover::before {
+   background-color: transparent !important;
 }
 </style>
