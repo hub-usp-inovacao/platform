d4cd0f08b0bfc77925540e21631846b334bffb0d
Luiz Carlos <luizcarlosdk@hotmail.com>
2024-12-19 12:15:51 -0300

    Change forms link
    Signed-off-by: Luiz Carlos <luizcarlosdk@hotmail.com>

--- frontend/modules/Catalog/pages/empresas.vue
+++ frontend/modules/Catalog/pages/empresas.vue
@@ -5,7 +5,7 @@
         v-model="search.term"
         title="Empresas"
         description="As Empresas DNA USP estão organizadas nesta plataforma por: CNAEs (Classificação Nacional de Atividades Econômicas), cidade, habitats de inovação e porte."
-        url="https://docs.google.com/forms/d/1q354be1_cPpeSIWVQkU2CXUpjUiyYuC0IU5W1_4W_zA/edit?usp=sharing" 
+        url="https://leginf.usp.br/?portaria=portaria-gr-no-7679-de-29-de-novembro-de-2021" 
         forms-call="Verique o regulamento de cadastro"
         second-url="https://docs.google.com/forms/d/1q354be1_cPpeSIWVQkU2CXUpjUiyYuC0IU5W1_4W_zA/edit?usp=sharing"
         second-call="Cadastre sua empresa aqui"
