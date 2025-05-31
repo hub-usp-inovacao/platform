10645e5af8774dbb264a3e025b0541f2dcbd894f
gabrielfsouza2002 <gabrielfsouza.araujo@usp.br>
2025-05-30 22:10:51 -0300

    Docs version temp - frontend with vitepress.

---
+++ frontend/docs/docs/.vitepress/config.ts
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vitepress'
+
+export default defineConfig({
+    base: '/platform/',
+    title: 'Hub USP Inovação',
+    description: 'Frontend documentation for Hub USP Inovação platform'
+})
