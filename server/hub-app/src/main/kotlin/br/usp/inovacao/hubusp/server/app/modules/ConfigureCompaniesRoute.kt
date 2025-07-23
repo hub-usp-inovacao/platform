d1a22393d3d94995961cdd60eb84010d4dc7945a
enzohideo <41664930+enzohideo@users.noreply.github.com>
2025-07-23 06:09:03 -0300

    server: hub-app: Email data received from POST /companies/company

--- server/hub-app/src/main/kotlin/br/usp/inovacao/hubusp/server/app/modules/ConfigureCompaniesRoute.kt
+++ server/hub-app/src/main/kotlin/br/usp/inovacao/hubusp/server/app/modules/ConfigureCompaniesRoute.kt
@@ -1,19 +1,39 @@
 package br.usp.inovacao.hubusp.server.app.modules
 
+import br.usp.inovacao.hubusp.mailer.Mail
+import br.usp.inovacao.hubusp.mailer.Mailer
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.Application
 import io.ktor.server.application.call
+import io.ktor.server.request.receiveText
 import io.ktor.server.response.respond
 import io.ktor.server.routing.post
 import io.ktor.server.routing.route
 import io.ktor.server.routing.routing
+import java.nio.file.Files
+import kotlin.io.path.writeText
 
-fun Application.configureCompaniesRoute() {
+fun Application.configureCompaniesRoute(mailer: Mailer, recipientList: List<String>) {
     routing {
         route("/companies") {
             post("/company") {
-                // TODO: Get data from POST request
+                // TODO: Deserialize JSON -> CompanyStep
                 // TODO: Validation, return constraint validation errors
+                val text = call.receiveText()
+
+                val file =
+                    Files.createTempFile("tmp-company", ".json").apply {
+                        writeText(text, Charsets.UTF_8)
+                    }
+
+                mailer.send(
+                    Mail(
+                        to = recipientList,
+                        subject = "Cadastro de companhia",
+                        body = "", // TODO: Make it optional
+                        attachments = listOf(Mail.Attachment("company.json", file.toFile())),
+                    ))
+
                 call.respond(HttpStatusCode.OK)
             }
         }
