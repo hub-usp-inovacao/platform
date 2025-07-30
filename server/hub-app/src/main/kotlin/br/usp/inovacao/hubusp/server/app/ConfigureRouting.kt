19102e48cf645addcf32ed201bcc778ad18f0203
enzohideo <41664930+enzohideo@users.noreply.github.com>
2025-07-30 00:25:10 -0300

    server: hub-app: Move route configuration to ConfigureRouting

--- server/hub-app/src/main/kotlin/br/usp/inovacao/hubusp/server/app/ConfigureRouting.kt
+++ server/hub-app/src/main/kotlin/br/usp/inovacao/hubusp/server/app/ConfigureRouting.kt
@@ -1,5 +1,10 @@
 package br.usp.inovacao.hubusp.server.app
 
+import br.usp.inovacao.hubusp.mailer.Mailer
+import br.usp.inovacao.hubusp.server.app.routing.configureCatalogRoute
+import br.usp.inovacao.hubusp.server.app.routing.configureCompaniesRoute
+import br.usp.inovacao.hubusp.server.app.routing.configureJourneyRoute
+import com.mongodb.client.MongoDatabase
 import io.ktor.server.application.Application
 import io.ktor.server.application.call
 import io.ktor.server.auth.authenticate
@@ -7,16 +12,21 @@ import io.ktor.server.response.respond
 import io.ktor.server.routing.get
 import io.ktor.server.routing.routing
 
-fun Application.configureRouting() {
-    routing {
-        get("/") {
-            call.respond("app root ok")
-        }
+fun Application.configureRouting(db: MongoDatabase) {
+    // TODO: Prepend /catalog to avoid conflicts
+    // (this would require updating Caddy to not strip /catalog)
+    configureCatalogRoute(db)
+    configureJourneyRoute(db)
+    configureCompaniesRoute(
+        Mailer(
+            environment.config.property("email.username").getString(),
+            environment.config.property("email.password").getString(),
+        ),
+        listOf(environment.config.property("email.devs").getString()),
+    )
 
-        authenticate {
-            get("/admin/") {
-                call.respond("admin root ok")
-            }
-        }
+    routing {
+        get("/") { call.respond("app root ok") }
+        authenticate { get("/admin/") { call.respond("admin root ok") } }
     }
 }
