78f1613469edc79d67da9b345757f1b1986fc2c4
enzohideo <41664930+enzohideo@users.noreply.github.com>
2025-07-30 00:25:10 -0300

    server: hub-app: Move route configuration to ConfigureRouting

--- server/hub-app/src/main/kotlin/br/usp/inovacao/hubusp/server/app/Application.kt
+++ server/hub-app/src/main/kotlin/br/usp/inovacao/hubusp/server/app/Application.kt
@@ -1,9 +1,5 @@
 package br.usp.inovacao.hubusp.server.app
 
-import br.usp.inovacao.hubusp.mailer.Mailer
-import br.usp.inovacao.hubusp.server.app.routing.configureCatalogRoute
-import br.usp.inovacao.hubusp.server.app.routing.configureCompaniesRoute
-import br.usp.inovacao.hubusp.server.app.routing.configureJourneyRoute
 import br.usp.inovacao.hubusp.server.persistence.configureDB
 import io.ktor.server.application.Application
 import io.ktor.server.netty.EngineMain
@@ -15,26 +11,13 @@ fun Application.module() {
     configureHttp()
     configureSecurity()
     configureSerialization()
-    configureRouting() // TODO: Remove this (unecessary routes)
-    configureCallLogging()
-
-    val db =
+    configureRouting(
         configureDB(
             protocol = environment.config.property("datasource.protocol").getString(),
             host = environment.config.property("datasource.host").getString(),
             port = environment.config.property("datasource.port").getString(),
             dbName = environment.config.property("datasource.dbName").getString(),
-        )
-
-    // TODO: Prepend /catalog to avoid conflicts
-    // (this would require updating Caddy to not strip /catalog)
-    configureCatalogRoute(db)
-    configureJourneyRoute(db)
-    configureCompaniesRoute(
-        Mailer(
-            environment.config.property("email.username").getString(),
-            environment.config.property("email.password").getString(),
         ),
-        listOf(environment.config.property("email.devs").getString()),
     )
+    configureCallLogging()
 }
