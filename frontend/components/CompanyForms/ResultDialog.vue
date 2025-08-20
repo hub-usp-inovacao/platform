78abd6f9ff2ba795ef33697be925d7ccaffbbaea
enzohideo <41664930+enzohideo@users.noreply.github.com>
2025-08-20 05:33:27 -0300

    frontend: pages/empresas/cadastrar: Add ResultDialog

---
+++ frontend/components/CompanyForms/ResultDialog.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-dialog v-model="show" persistent max-width="500">
+    <v-card>
+      <v-card-title class="text-h5">
+        {{ title }}
+      </v-card-title>
+      <v-card-text>
+        {{ message }}
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn color="green darken-1" text @click="$emit('ok-click')">
+          Ok!
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  props: {
+    show: Boolean,
+    success: Boolean,
+    title: {
+      type: String,
+      default: "",
+    },
+    message: {
+      type: String,
+      default: "",
+    },
+  },
+};
+</script>
