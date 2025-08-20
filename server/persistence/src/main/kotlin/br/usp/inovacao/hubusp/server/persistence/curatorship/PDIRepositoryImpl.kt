3d9ff824c40e3f50a87a5b5633b44465141cdd0e
GuiHVC <guilherme.cunha1411.mc@gmail.com>
2025-08-20 10:35:57 -0300

    Remove unused imports

--- server/persistence/src/main/kotlin/br/usp/inovacao/hubusp/server/persistence/curatorship/PDIRepositoryImpl.kt
+++ server/persistence/src/main/kotlin/br/usp/inovacao/hubusp/server/persistence/curatorship/PDIRepositoryImpl.kt
@@ -7,7 +7,6 @@ import com.mongodb.client.MongoCollection
 import com.mongodb.client.MongoDatabase
 import com.mongodb.client.model.Filters
 import org.litote.kmongo.getCollection
-import java.time.LocalDateTime
 
 class PDIRepositoryImpl(
     db: MongoDatabase
