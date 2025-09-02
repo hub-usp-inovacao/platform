76fc14ed25438031aa1b203fd280f8e096480fa4
enzohideo <41664930+enzohideo@users.noreply.github.com>
2025-09-02 04:53:48 -0300

    server: curatorship/register: Fix CompanyStepTest

--- server/curatorship/src/test/kotlin/br/usp/inovacao/hubusp/curatorship/register/CompanyStepTestHelp.kt
+++ server/curatorship/src/test/kotlin/br/usp/inovacao/hubusp/curatorship/register/CompanyStepTestHelp.kt
@@ -1,42 +1,47 @@
 package br.usp.inovacao.hubusp.curatorship.register
 
-fun AboutCompanyStep.toRow(): List<String?> = listOf(
-    description,
-    logo,
-    services.joinToString(";"),
-    technologies.joinToString(";"),
-    site,
-    odss.joinToString(";"),
-    socialMedia.joinToString(";")
-)
+import br.usp.inovacao.hubusp.curatorship.register.step.AboutCompanyStep
 
-fun CompanyStep.toRow(): List<String?> = listOf(
-    about.description,
-    about.logo,
-    about.services.joinToString(";"),
-    about.technologies.joinToString(";"),
-    about.site,
-    about.odss.joinToString(";"),
-    about.socialMedia.joinToString(";")
-)
+// TODO: toJSON
+
+fun AboutCompanyStep.toRow(): List<String?> =
+    listOf(
+        description,
+        logo,
+        services.joinToString(";"),
+        technologies.joinToString(";"),
+        site,
+        odss.joinToString(";"),
+        socialMedias.joinToString(";"),
+    )
+
+fun CompanyStep.toRow(): List<String?> =
+    listOf(
+        about.description,
+        about.logo,
+        about.services.joinToString(";"),
+        about.technologies.joinToString(";"),
+        about.site,
+        about.odss.joinToString(";"),
+        about.socialMedias.joinToString(";"),
+    )
 
 class CompanyStepTestHelp {
 
     companion object {
 
-        val VALID_ABOUT = AboutCompanyStep(
-            description = "Empresa para testes",
-            logo = "https://www.logodofulanodetal.com",
-            services = setOf("Website","Blog"),
-            technologies = setOf("Aplicativos","Biomateriais"),
-            site = "https://www.fulanodetal.com",
-            odss = setOf("foo","bar"),
-            socialMedia = setOf("https://www.fulanodetal.com")
-        )
+        val VALID_ABOUT =
+            AboutCompanyStep(
+                description = "Empresa para testes",
+                logo = "https://www.logodofulanodetal.com",
+                services = setOf("Website", "Blog"),
+                technologies = setOf("Aplicativos", "Biomateriais"),
+                site = "https://www.fulanodetal.com",
+                odss = setOf("1 - Erradicação da Pobreza"),
+                socialMedias = setOf("https://www.fulanodetal.com"),
+            )
 
-        val VALID_REGISTER_RECORD = CompanyStep(
-            about = VALID_ABOUT
-        )
+        val VALID_REGISTER_RECORD = CompanyStep(about = VALID_ABOUT)
 
         fun validRegisterAndInvalidRegister(): List<List<String?>> {
             val validRegister = VALID_REGISTER_RECORD.toRow()
@@ -44,7 +49,7 @@ class CompanyStepTestHelp {
             invalidRegister[0] = ""
             return listOf(validRegister, invalidRegister)
         }
+
         val validRegister: List<String?> = VALID_REGISTER_RECORD.toRow()
     }
-
 }
