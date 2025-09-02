21e467d19916a3e97a03a12a9f82f607c3fcbf5c
enzohideo <41664930+enzohideo@users.noreply.github.com>
2025-09-02 09:22:38 -0300

    server: curatorship/register: Validate PartnerStep

---
+++ server/curatorship/src/test/kotlin/br/usp/inovacao/hubusp/curatorship/register/step/PartnerStepTest.kt
@@ -0,0 +1,69 @@
+package br.usp.inovacao.hubusp.curatorship.register.step
+
+import kotlin.test.Test
+import kotlin.test.assertFailsWith
+
+class PartnerStepTest {
+    @Test
+    fun `it does not throws an error when PartnerStep is valid`() {
+        validStep.validate()
+    }
+
+    @Test
+    fun `it does not throws an error when Partner is valid`() {
+        validPartner.validate()
+    }
+
+    @Test
+    fun `it throws an error when partner has blank name`() {
+
+        assertFailsWith<StepValidationException> { validPartner.copy(name = "").validate() }
+    }
+
+    @Test
+    fun `it does not throw an error when partner has blank nusp`() {
+
+        validPartner.copy(nusp = "").validate()
+    }
+
+    @Test
+    fun `it throws an error when partner has blank bond`() {
+
+        assertFailsWith<StepValidationException> { validPartner.copy(bond = "").validate() }
+    }
+
+    @Test
+    fun `it throws an error when partner has blank unit`() {
+
+        assertFailsWith<StepValidationException> { validPartner.copy(unit = "").validate() }
+    }
+
+    @Test
+    fun `it throws an error when partner has invalid email`() {
+
+        assertFailsWith<StepValidationException> {
+            validPartner.copy(email = "hello world").validate()
+        }
+    }
+
+    @Test
+    fun `it throws an error when partner has phone without numbers`() {
+
+        assertFailsWith<StepValidationException> {
+            validPartner.copy(phone = "hello world").validate()
+        }
+    }
+
+    private val validPartner =
+        Partner(
+            name = "Some partner",
+            nusp = "1234567",
+            bond = "Some bond",
+            unit = "Some unit",
+            email = "test@example.com",
+            phone = "(11) 99999-9999",
+            role = "Some role",
+        )
+
+    private val validStep = listOf(validPartner)
+}
