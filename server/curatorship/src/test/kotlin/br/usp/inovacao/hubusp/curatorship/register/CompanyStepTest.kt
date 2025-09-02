8882679d243fc0c072e0802717fdb936d0c27b87
enzohideo <41664930+enzohideo@users.noreply.github.com>
2025-09-02 04:53:48 -0300

    server: curatorship/register: Fix CompanyStepTest

--- server/curatorship/src/test/kotlin/br/usp/inovacao/hubusp/curatorship/register/CompanyStepTest.kt
+++ server/curatorship/src/test/kotlin/br/usp/inovacao/hubusp/curatorship/register/CompanyStepTest.kt
@@ -2,13 +2,20 @@ package br.usp.inovacao.hubusp.curatorship.register
 
 import kotlin.test.Test
 import kotlin.test.assertFailsWith
+import kotlin.test.assertEquals
+import br.usp.inovacao.hubusp.curatorship.register.step.StepValidationException
 
+// TODO: Separate tests for each step
 class CompanyStepTest {
+    @Test
+    fun `it does not throws an error when AboutCompanyStep is valid`() {
+        validAbout.validate()
+    }
 
     @Test
-    fun `it throws an error when about company description is empty`() {
-        assertFailsWith<ValidationException> {
-            validAbout.copy(description = "")
+    fun `it throws an error when AboutCompanyStep description is empty`() {
+        assertFailsWith<StepValidationException> {
+            validAbout.copy(description = "").validate()
         }
     }
 
