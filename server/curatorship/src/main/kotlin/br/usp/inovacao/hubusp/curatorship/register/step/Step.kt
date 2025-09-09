c16e60e98ab508eda27e9bb557d6bc65431d557d
enzohideo <41664930+enzohideo@users.noreply.github.com>
2025-09-09 20:48:27 -0300

    server: curatorship/register/step/Step: Serialize to snake case

--- server/curatorship/src/main/kotlin/br/usp/inovacao/hubusp/curatorship/register/step/Step.kt
+++ server/curatorship/src/main/kotlin/br/usp/inovacao/hubusp/curatorship/register/step/Step.kt
@@ -1,26 +1,16 @@
 package br.usp.inovacao.hubusp.curatorship.register.step
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
-
-// TODO: Serialize to snake_case JSON
-//
-// CompanyData -> company_data
-// Investment -> about_company
-// Revenue -> investment
-// Incubation -> incubation
-// Staff -> colaborators
-// CompanyData -> revenue
-// Partner -> partners
-// DnaUspStamp -> dna_usp_stamp
 
 @Serializable
 enum class Step {
-    CompanyData,
-    Investment,
-    Revenue,
-    Incubation,
-    DNAUspStamp,
-    Staff,
-    Partner,
-    AboutCompany,
+    @SerialName("company_data") CompanyData,
+    @SerialName("investment") Investment,
+    @SerialName("revenue") Revenue,
+    @SerialName("incubation") Incubation,
+    @SerialName("dna_usp_stamp") DNAUspStamp,
+    @SerialName("staff") Staff,
+    @SerialName("partner") Partner,
+    @SerialName("about_company") AboutCompany,
 }
