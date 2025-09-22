ffd1608464ef75e15d3aee04f6bbf2d91b04009c
enzohideo <41664930+enzohideo@users.noreply.github.com>
2025-09-21 23:33:49 -0300

    server: curatorship: CompanyFormValidationException: Add typealias to ErrorsPerStep

--- server/curatorship/src/main/kotlin/br/usp/inovacao/hubusp/curatorship/register/CompanyFormValidationException.kt
+++ server/curatorship/src/main/kotlin/br/usp/inovacao/hubusp/curatorship/register/CompanyFormValidationException.kt
@@ -2,5 +2,7 @@ package br.usp.inovacao.hubusp.curatorship.register
 
 import br.usp.inovacao.hubusp.curatorship.register.step.Step
 
-data class CompanyFormValidationException(val errorsPerStep: Map<Step, List<String>>) :
+data class CompanyFormValidationException(val errorsPerStep: ErrorsPerStep) :
     RuntimeException(errorsPerStep.toList().joinToString("|"))
+
+typealias ErrorsPerStep = Map<Step, List<String>>
