88d6ed5c4fcbdad65e6fdc90992fb675b5f3b840
enzohideo <41664930+enzohideo@users.noreply.github.com>
2025-09-29 03:44:13 -0300

    server: sheets/SpreadsheetWriter: Return updatedRange instead of AppendValuesResponse

--- server/sheets/src/main/kotlin/br/usp/inovacao/hubusp/sheets/SpreadsheetWriter.kt
+++ server/sheets/src/main/kotlin/br/usp/inovacao/hubusp/sheets/SpreadsheetWriter.kt
@@ -1,13 +1,14 @@
 package br.usp.inovacao.hubusp.sheets
 
 import com.google.api.services.sheets.v4.model.AppendValuesResponse
+import com.google.api.services.sheets.v4.model.UpdateValuesResponse
 import java.io.IOException
 
 class SpreadsheetWriter(val sheetId: String, val sheetName: String) {
-    fun append(matrix: Matrix<String?>, startingCell: String = "A1"): AppendValuesResponse {
+    fun append(matrix: Matrix<String?>, startingCell: String = "A1"): String {
         try {
             val range = "'${sheetName}':${startingCell}"
-            return SpreadsheetService.append(sheetId, range, matrix)
+            return SpreadsheetService.append(sheetId, range, matrix).updates.updatedRange
         } catch (_: IOException) {
             throw SpreasheetException(
                 sheetName,
