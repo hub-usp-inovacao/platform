f62961f66b7e32e7609d11eea12d9e174ecd0c1b
enzohideo <41664930+enzohideo@users.noreply.github.com>
2025-10-06 01:18:12 -0300

    frontend: pages/empresas/cadastrar: Remove company_forms/errors from mapActions

--- frontend/pages/empresas/cadastrar.vue
+++ frontend/pages/empresas/cadastrar.vue
@@ -47,7 +47,6 @@ export default {
   methods: {
     ...mapActions({
       registerCompanyForm: "company_forms/registerCompanyForm",
-      errors: "company_forms/errors",
     }),
 
     async onStepperFinish() {
