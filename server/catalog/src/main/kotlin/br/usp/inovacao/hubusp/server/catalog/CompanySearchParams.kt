7e4835dd5e01bf1f6f73ac97b837d4c319d1d677
enzohideo <41664930+enzohideo@users.noreply.github.com>
2025-11-03 09:52:10 -0300

    server: {catalog,persistence}: CompanySearchParams: Allow it to filter by cnpj

--- server/catalog/src/main/kotlin/br/usp/inovacao/hubusp/server/catalog/CompanySearchParams.kt
+++ server/catalog/src/main/kotlin/br/usp/inovacao/hubusp/server/catalog/CompanySearchParams.kt
@@ -1,6 +1,7 @@
 package br.usp.inovacao.hubusp.server.catalog
 
 data class CompanySearchParams(
+    val cnpj: String? = null,
     val areaMajors: Set<String> = emptySet(),
     val areaMinors: Set<String> = emptySet(),
     val unity: String? = null,
