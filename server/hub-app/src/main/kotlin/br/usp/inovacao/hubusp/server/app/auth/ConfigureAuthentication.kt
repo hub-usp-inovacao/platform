87136fd0345ffcf49e2243cc59ac42223d301442
enzohideo <41664930+enzohideo@users.noreply.github.com>
2025-11-03 00:54:52 -0300

    server: hub-app/auth/ConfigureAuthentication: Send response if auth fails

--- server/hub-app/src/main/kotlin/br/usp/inovacao/hubusp/server/app/auth/ConfigureAuthentication.kt
+++ server/hub-app/src/main/kotlin/br/usp/inovacao/hubusp/server/app/auth/ConfigureAuthentication.kt
@@ -3,10 +3,12 @@ package br.usp.inovacao.hubusp.server.app.auth
 import br.usp.inovacao.hubusp.config.Configuration
 import com.auth0.jwt.JWT
 import com.auth0.jwt.algorithms.Algorithm
+import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.Application
 import io.ktor.server.auth.authentication
 import io.ktor.server.auth.jwt.JWTPrincipal
 import io.ktor.server.auth.jwt.jwt
+import io.ktor.server.response.respond
 
 fun Application.configureAuthentication() {
     val secret = Configuration.jwt.secret
@@ -31,6 +33,13 @@ fun Application.configureAuthentication() {
                 } else {
                     null
                 }
+            }
+
+            challenge { defaultScheme, _ ->
+                call.respond(
+                    HttpStatusCode.Unauthorized,
+                    "Token for ${defaultScheme} is not valid or has expired",
+                )
             }
         }
     }
