f6cb04c16dcd52b2fa1c0fe8a76f52e4a30241a5
enzohideo <41664930+enzohideo@users.noreply.github.com>
2025-11-04 12:33:32 -0300

    server: hub-app/auth: Add StaticJWTAuthProvider interface

--- server/hub-app/src/main/kotlin/br/usp/inovacao/hubusp/server/app/auth/AuthProvider.kt
+++ server/hub-app/src/main/kotlin/br/usp/inovacao/hubusp/server/app/auth/AuthProvider.kt
@@ -1,14 +1,27 @@
 package br.usp.inovacao.hubusp.server.app.auth
 
+import com.auth0.jwt.JWTVerifier
 import com.auth0.jwt.interfaces.DecodedJWT
 import com.auth0.jwt.interfaces.Payload
+import io.ktor.server.application.ApplicationCall
+import io.ktor.server.auth.Principal
+import io.ktor.server.auth.jwt.JWTChallengeContext
+import io.ktor.server.auth.jwt.JWTCredential
 
 interface AuthProvider {
     abstract fun createToken(): String
 }
 
-interface AuthProviderName {
+interface StaticAuthProvider {
     abstract val providerName: String
+}
+
+interface StaticJWTAuthProvider {
+    abstract val realm: String
+    abstract val verifier: JWTVerifier
+    abstract val validate: suspend ApplicationCall.(JWTCredential) -> Principal?
+    abstract val challenge:
+        suspend JWTChallengeContext.(defaultScheme: String, realm: String) -> Unit
 }
 
 interface FromJWT<out Self> {
--- server/hub-app/src/main/kotlin/br/usp/inovacao/hubusp/server/app/auth/CompanyJWT.kt
+++ server/hub-app/src/main/kotlin/br/usp/inovacao/hubusp/server/app/auth/CompanyJWT.kt
@@ -5,7 +5,14 @@ import com.auth0.jwt.JWT
 import com.auth0.jwt.algorithms.Algorithm
 import com.auth0.jwt.interfaces.DecodedJWT
 import com.auth0.jwt.interfaces.Payload
+import io.ktor.http.HttpStatusCode
+import io.ktor.server.application.ApplicationCall
+import io.ktor.server.auth.Principal
+import io.ktor.server.auth.jwt.JWTChallengeContext
+import io.ktor.server.auth.jwt.JWTCredential
+import io.ktor.server.auth.jwt.JWTPrincipal
 import io.ktor.server.auth.jwt.jwt
+import io.ktor.server.response.respond
 
 @kotlinx.serialization.Serializable
 class CompanyJWT(val cnpj: String) : AuthProvider {
@@ -16,8 +23,34 @@ class CompanyJWT(val cnpj: String) : AuthProvider {
             .withClaim("cnpj", this.cnpj)
             .sign(Algorithm.HMAC256(Configuration.jwt.secret))
 
-    companion object : AuthProviderName, FromJWT<CompanyJWT> {
+    companion object : StaticAuthProvider, StaticJWTAuthProvider, FromJWT<CompanyJWT> {
         override val providerName = "company-auth-jwt"
+
+        override val realm = Configuration.jwt.realm
+
+        override val verifier =
+            JWT.require(Algorithm.HMAC256(Configuration.jwt.secret))
+                .withAudience(Configuration.jwt.audience)
+                .withIssuer(Configuration.jwt.issuer)
+                .build()
+
+        override val validate: suspend ApplicationCall.(JWTCredential) -> Principal? =
+            { credential ->
+                if (credential.payload.getClaim("cnpj").asString() != "") {
+                    JWTPrincipal(credential.payload)
+                } else {
+                    null
+                }
+            }
+
+        override val challenge:
+            suspend JWTChallengeContext.(defaultScheme: String, realm: String) -> Unit =
+            { defaultScheme, _ ->
+                call.respond(
+                    HttpStatusCode.Unauthorized,
+                    "Token for ${defaultScheme} is not valid or has expired",
+                )
+            }
 
         fun fromData(cnpj: String?): CompanyJWT? {
             return if (cnpj != null) {
