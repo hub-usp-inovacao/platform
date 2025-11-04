2c630ca57bf77c77ceaad7e2478979aa02019984
enzohideo <41664930+enzohideo@users.noreply.github.com>
2025-11-04 12:33:32 -0300

    server: hub-app/auth: Add StaticJWTAuthProvider interface

--- server/hub-app/src/main/kotlin/br/usp/inovacao/hubusp/server/app/auth/ConfigureAuthentication.kt
+++ server/hub-app/src/main/kotlin/br/usp/inovacao/hubusp/server/app/auth/ConfigureAuthentication.kt
@@ -1,46 +1,16 @@
 package br.usp.inovacao.hubusp.server.app.auth
 
-import br.usp.inovacao.hubusp.config.Configuration
-import com.auth0.jwt.JWT
-import com.auth0.jwt.algorithms.Algorithm
-import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.Application
 import io.ktor.server.auth.authentication
-import io.ktor.server.auth.jwt.JWTPrincipal
 import io.ktor.server.auth.jwt.jwt
-import io.ktor.server.response.respond
 
 fun Application.configureAuthentication() {
-    val secret = Configuration.jwt.secret
-    val issuer = Configuration.jwt.issuer
-    val audience = Configuration.jwt.audience
-    val myRealm = Configuration.jwt.realm
-
     authentication {
         jwt(CompanyJWT.providerName) {
-            realm = myRealm
-
-            verifier(
-                JWT.require(Algorithm.HMAC256(secret))
-                    .withAudience(audience)
-                    .withIssuer(issuer)
-                    .build(),
-            )
-
-            validate { credential ->
-                if (credential.payload.getClaim("cnpj").asString() != "") {
-                    JWTPrincipal(credential.payload)
-                } else {
-                    null
-                }
-            }
-
-            challenge { defaultScheme, _ ->
-                call.respond(
-                    HttpStatusCode.Unauthorized,
-                    "Token for ${defaultScheme} is not valid or has expired",
-                )
-            }
+            realm = CompanyJWT.realm
+            verifier(CompanyJWT.verifier)
+            validate(CompanyJWT.validate)
+            challenge(CompanyJWT.challenge)
         }
     }
 }
