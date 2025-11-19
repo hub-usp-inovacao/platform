b7bc13d707abdfef63902d50b52269344eaa7137
enzohideo <41664930+enzohideo@users.noreply.github.com>
2025-11-19 03:45:27 -0300

    server: sheets/build.gradle: Use version catalog

--- server/sheets/build.gradle.kts
+++ server/sheets/build.gradle.kts
@@ -1,6 +1,3 @@
-val kotlin_version: String by project
-val mockk_version: String by project
-
 plugins {
     kotlin("jvm")
     id("org.jetbrains.dokka")
@@ -14,10 +11,7 @@ version = "0.0.1"
 repositories { mavenCentral() }
 
 dependencies {
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
-    testImplementation("io.mockk:mockk:$mockk_version")
-
-    implementation("com.google.auth:google-auth-library-oauth2-http:1.37.1")
-    implementation("com.google.api-client:google-api-client:2.7.2")
-    implementation("com.google.apis:google-api-services-sheets:v4-rev20250211-2.0.0")
+    implementation(libs.bundles.google.sheets)
+    testImplementation(libs.junit)
+    testImplementation(libs.mockk)
 }
