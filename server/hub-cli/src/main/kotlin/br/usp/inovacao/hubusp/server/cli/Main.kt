46b687ce1e64ebfdd0d2bc75298aa948a056da70
enzohideo <41664930+enzohideo@users.noreply.github.com>
2025-11-19 16:15:54 -0300

    server: hub-cli: Use lazy initialization

--- server/hub-cli/src/main/kotlin/br/usp/inovacao/hubusp/server/cli/Main.kt
+++ server/hub-cli/src/main/kotlin/br/usp/inovacao/hubusp/server/cli/Main.kt
@@ -1,17 +1,25 @@
 package br.usp.inovacao.hubusp.server.cli
 
 import br.usp.inovacao.hubusp.server.cli.commands.Test
-import br.usp.inovacao.hubusp.server.cli.commands.curatorship.RefreshPDI
 import br.usp.inovacao.hubusp.server.cli.commands.curatorship.RefreshCompany
-import br.usp.inovacao.hubusp.server.cli.commands.curatorship.RefreshResearcher
 import br.usp.inovacao.hubusp.server.cli.commands.curatorship.RefreshDiscipline
 import br.usp.inovacao.hubusp.server.cli.commands.curatorship.RefreshInitiative
+import br.usp.inovacao.hubusp.server.cli.commands.curatorship.RefreshPDI
 import br.usp.inovacao.hubusp.server.cli.commands.curatorship.RefreshPatent
+import br.usp.inovacao.hubusp.server.cli.commands.curatorship.RefreshResearcher
 import com.github.ajalt.clikt.core.subcommands
 
 fun main(args: Array<String>) {
     CLI().run {
-        subcommands(Test(), RefreshPDI(), RefreshCompany(), RefreshDiscipline(), RefreshResearcher(), RefreshInitiative(), RefreshPatent())
+        subcommands(
+            Test(),
+            RefreshPDI(),
+            RefreshCompany(),
+            RefreshDiscipline(),
+            RefreshResearcher(),
+            RefreshInitiative(),
+            RefreshPatent(),
+        )
         main(args)
     }
 }
--- server/hub-cli/src/main/kotlin/br/usp/inovacao/hubusp/server/cli/commands/curatorship/RefreshCompany.kt
+++ server/hub-cli/src/main/kotlin/br/usp/inovacao/hubusp/server/cli/commands/curatorship/RefreshCompany.kt
@@ -3,34 +3,22 @@ package br.usp.inovacao.hubusp.server.cli.commands.curatorship
 import br.usp.inovacao.hubusp.config.Configuration
 import br.usp.inovacao.hubusp.curatorship.Mailer
 import br.usp.inovacao.hubusp.curatorship.sheets.SpreadsheetReader
-import br.usp.inovacao.hubusp.server.persistence.configureDB
 import br.usp.inovacao.hubusp.server.persistence.curatorship.CompanyErrorRepositoryImpl
 import br.usp.inovacao.hubusp.server.persistence.curatorship.CompanyRepositoryImpl
 import com.github.ajalt.clikt.core.CliktCommand
 
 class RefreshCompany : CliktCommand() {
-    private val refreshCompany: br.usp.inovacao.hubusp.curatorship.sheets.RefreshCompany
-
-    init {
-        val db =
-            configureDB(
-                Configuration.database.protocol,
-                Configuration.database.host,
-                Configuration.database.port,
-                Configuration.database.dbName,
-            )
-
-        refreshCompany =
-            br.usp.inovacao.hubusp.curatorship.sheets.RefreshCompany(
-                mailer =
-                    Mailer(
-                        Configuration.email.username,
-                        Configuration.email.password,
-                    ),
-                spreadsheetReader = SpreadsheetReader(),
-                companyRepository = CompanyRepositoryImpl(db),
-                companyErrorRepository = CompanyErrorRepositoryImpl(db),
-            )
+    private val refreshCompany: br.usp.inovacao.hubusp.curatorship.sheets.RefreshCompany by lazy {
+        br.usp.inovacao.hubusp.curatorship.sheets.RefreshCompany(
+            mailer =
+                Mailer(
+                    Configuration.email.username,
+                    Configuration.email.password,
+                ),
+            spreadsheetReader = SpreadsheetReader(),
+            companyRepository = CompanyRepositoryImpl(Refresh.db),
+            companyErrorRepository = CompanyErrorRepositoryImpl(Refresh.db),
+        )
     }
 
     override fun run() {
