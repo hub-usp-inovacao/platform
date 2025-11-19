c7b120d11eb1520365b4e09c69cd4a67e1bc922d
enzohideo <41664930+enzohideo@users.noreply.github.com>
2025-11-19 03:37:06 -0300

    server: hub-cli/build.gradle: Use version catalog

--- server/hub-cli/build.gradle.kts
+++ server/hub-cli/build.gradle.kts
@@ -1,34 +1,20 @@
-val kotlin_version: String by project
-val logback_version: String by project
-val mockk_version: String by project
-val clikt_version: String by project
-val kmongo_version: String by project
-
 plugins {
     application
     kotlin("jvm")
     kotlin("plugin.serialization")
-    id("com.github.johnrengelman.shadow") version "7.0.0"
+    alias(libs.plugins.shadow)
     id("org.jetbrains.dokka")
 }
 
-application {
-    mainClass.set("br.usp.inovacao.hubusp.server.cli.MainKt")
-}
+application { mainClass.set("br.usp.inovacao.hubusp.server.cli.MainKt") }
 
 tasks {
     shadowJar {
-        manifest {
-            attributes(
-                Pair("Main-Class", "br.usp.inovacao.hubusp.server.cli.MainKt")
-            )
-        }
+        manifest { attributes(Pair("Main-Class", "br.usp.inovacao.hubusp.server.cli.MainKt")) }
     }
 }
 
-repositories {
-    mavenCentral()
-}
+repositories { mavenCentral() }
 
 dependencies {
     implementation(project(":config"))
@@ -36,9 +22,10 @@ dependencies {
     implementation(project(":techtransfer"))
     implementation(project(":persistence"))
 
-    implementation("org.litote.kmongo:kmongo-serialization:$kmongo_version")
-    implementation("com.github.ajalt.clikt:clikt:$clikt_version")
-    implementation("ch.qos.logback:logback-classic:$logback_version")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
-    testImplementation("io.mockk:mockk:$mockk_version")
+    implementation(libs.kmongo.serialization)
+    implementation(libs.clikt)
+
+    implementation(libs.logback)
+    testImplementation(libs.junit)
+    testImplementation(libs.mockk)
 }
