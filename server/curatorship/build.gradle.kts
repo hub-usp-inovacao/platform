c14e1be9a89ff61f39a036f6d54a32aa96c1bd78
enzohideo <41664930+enzohideo@users.noreply.github.com>
2025-11-19 03:05:06 -0300

    server: curatorship/build.gradle: Use version catalog
    ktor-client isn't even used, so I removed the dependency

--- server/curatorship/build.gradle.kts
+++ server/curatorship/build.gradle.kts
@@ -1,11 +1,3 @@
-val kotlinx_serialization_version: String by project
-val kotlin_version: String by project
-val logback_version: String by project
-val mockk_version: String by project
-val ktor_version : String by project
-val coroutines_version: String by project
-val valiktor_version: String by project
-
 plugins {
     kotlin("jvm")
     kotlin("plugin.serialization")
@@ -14,28 +6,21 @@ plugins {
 }
 
 group = "br.usp.inovacao.hubusp.curatorship"
+
 version = "0.0.1"
 
-repositories {
-    mavenCentral()
-}
+repositories { mavenCentral() }
 
 dependencies {
     implementation(project(":config"))
     implementation(project(":mailer"))
 
-    implementation("org.valiktor:valiktor-core:$valiktor_version")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutines_version")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:$kotlinx_serialization_version")
-    implementation("ch.qos.logback:logback-classic:$logback_version")
-    implementation("io.ktor:ktor-client-core:$ktor_version")
-    implementation("io.ktor:ktor-client-cio:$ktor_version")
-    implementation("io.ktor:ktor-client-content-negotiation:$ktor_version")
-    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktor_version")
-    implementation("it.skrape:skrapeit:1.3.0-alpha.1")
-    implementation("com.google.auth:google-auth-library-oauth2-http:1.37.1")
-    implementation("com.google.api-client:google-api-client:2.7.2")
-    implementation("com.google.apis:google-api-services-sheets:v4-rev20250211-2.0.0")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
-    testImplementation("io.mockk:mockk:$mockk_version")
+    implementation(libs.valiktor.core)
+    implementation(libs.kotlinx.serialization.json)
+    implementation(libs.skrapeit)
+    implementation(libs.bundles.google.sheets)
+    implementation(libs.logback)
+
+    testImplementation(libs.junit)
+    testImplementation(libs.mockk)
 }
