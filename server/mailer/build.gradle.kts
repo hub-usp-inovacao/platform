5e8b70930030171e8253ca67a0fa6300e891d782
enzohideo <41664930+enzohideo@users.noreply.github.com>
2025-11-19 03:39:24 -0300

    server: mailer/build.gradle: Use version catalog

--- server/mailer/build.gradle.kts
+++ server/mailer/build.gradle.kts
@@ -1,8 +1,3 @@
-val kotlin_version: String by project
-val logback_version: String by project
-val mockk_version: String by project
-val javax_mail_version: String by project
-
 plugins {
     kotlin("jvm")
     id("org.jetbrains.dokka")
@@ -10,15 +5,14 @@ plugins {
 }
 
 group = "br.usp.inovacao.hubusp.mailer"
+
 version = "0.0.1"
 
-repositories {
-    mavenCentral()
-}
+repositories { mavenCentral() }
 
 dependencies {
-    implementation("ch.qos.logback:logback-classic:$logback_version")
-    implementation("javax.mail:mail:$javax_mail_version")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
-    testImplementation("io.mockk:mockk:$mockk_version")
+    implementation(libs.javax.mail)
+    implementation(libs.logback)
+    testImplementation(libs.junit)
+    testImplementation(libs.mockk)
 }
