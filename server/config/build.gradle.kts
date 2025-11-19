fde89428c98edf42092109d64d7944a578d2d5ca
enzohideo <41664930+enzohideo@users.noreply.github.com>
2025-11-19 02:47:06 -0300

    server: config/build.gradle: Use version catalog

--- server/config/build.gradle.kts
+++ server/config/build.gradle.kts
@@ -1,6 +1,3 @@
-val kotlin_version: String by project
-val hoplite_version: String by project
-
 plugins {
     kotlin("jvm")
     id("org.jetbrains.dokka")
@@ -14,7 +11,6 @@ version = "0.0.1"
 repositories { mavenCentral() }
 
 dependencies {
-    implementation("com.sksamuel.hoplite:hoplite-core:$hoplite_version")
-    implementation("com.sksamuel.hoplite:hoplite-hocon:$hoplite_version")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+    implementation(libs.bundles.hoplite)
+    testImplementation(libs.junit)
 }
