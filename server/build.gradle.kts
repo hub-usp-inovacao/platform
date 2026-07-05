305b437ce30f78bc6ce2b2f8e8067962f812d873
Fernando Drekner de Souza <fernandodrekner@usp.br>
2026-07-05 16:51:15 -0300

    feat: adds test coverage

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.jvm)
     alias(libs.plugins.serialization)
     alias(libs.plugins.dokka)
+    alias(libs.plugins.kover)
 }
 
 repositories {
@@ -9,4 +10,28 @@ repositories {
     maven { url = uri("https://maven.pkg.jetbrains.space/public/p/ktor/eap") }
 }
 
-dependencies { allprojects { dokka(project) } }
+dependencies {
+    allprojects { dokka(project) }
+
+    kover(project(":catalog"))
+    kover(project(":config"))
+    kover(project(":curatorship"))
+    kover(project(":discovery"))
+    kover(project(":hub-app"))
+    kover(project(":hub-cli"))
+    kover(project(":mailer"))
+    kover(project(":persistence"))
+    kover(project(":sheets"))
+    kover(project(":techtransfer"))
+}
+
+
+kover {
+    reports {
+        verify {
+            rule {
+                minBound(0)
+            }
+        }
+    }
+}
