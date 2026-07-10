be17c7bed2f528f6b97545a85037adf35885d6e4
Fernando Drekner de Souza <fernandodrekner@usp.br>
2026-07-09 22:02:45 -0300

    feat: add test coverage for kotlin using kover

---
+++ dockers.sh
@@ -0,0 +1,6 @@
+sudo docker compose run catalogcli refresh-pdi &
+sudo docker compose run catalogcli refresh-company &
+sudo docker compose run catalogcli refresh-researcher &
+sudo docker compose run catalogcli refresh-initiative &
+sudo docker compose run catalogcli refresh-patent &
+sudo docker compose run catalogcli refresh-discipline
